c15af8d513ab54cfdcae46bce0eea6ff6a92af32
duanyytop <duanyytop@gmail.com>
2018-12-21 19:56:48 +0800

    Add unit test case for utils module

---
+++ utils/src/test/java/org/nervos/ckb/utils/AssertionsTest.java
@@ -0,0 +1,18 @@
+package org.nervos.ckb.utils;
+
+import org.junit.Test;
+
+import static org.nervos.ckb.utils.Assertions.verifyPrecondition;
+
+public class AssertionsTest {
+
+    @Test
+    public void testVerifyPrecondition() {
+        verifyPrecondition(true, "");
+    }
+
+    @Test(expected = RuntimeException.class)
+    public void testVerifyPreconditionFailure() {
+        verifyPrecondition(false, "");
+    }
+}
