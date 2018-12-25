2eb0a0ced456cbdcbfa9b389cd088621b2f022e3
duanyytop <duanyytop@gmail.com>
2018-12-25 14:42:11 +0800

    add rpc request examples and rename APITest.java to APITest.example

--- core/src/main/java/org/nervos/ckb/example/APIClient.java
+++ core/src/main/java/org/nervos/ckb/example/APIClient.java
@@ -2,10 +2,16 @@ package org.nervos.ckb.example;
 
 import com.google.gson.Gson;
 import org.nervos.ckb.response.item.Block;
+import org.nervos.ckb.response.item.Cell;
+import org.nervos.ckb.response.item.Header;
+import org.nervos.ckb.response.item.Transaction;
 import org.nervos.ckb.service.HttpService;
 import org.nervos.ckb.service.CKBService;
 
 import java.io.IOException;
+import java.math.BigInteger;
+import java.util.Collections;
+import java.util.List;
 
 public class APIClient {
 
@@ -15,7 +21,7 @@ public class APIClient {
 
 
     static {
-        HttpService.setDebug(true);
+        HttpService.setDebug(false);
         ckbService = CKBService.build(new HttpService(NODE_URL));
     }
 
@@ -26,6 +32,34 @@ public class APIClient {
 
         Block block = ckbService.getBlock(blockHash).send().getBlock();
         System.out.println("Second block is " + new Gson().toJson(block));
+
+        String transactionHash = block.transactions.get(0).hash;
+        Transaction transaction = ckbService.getTransaction(transactionHash).send().getTransaction().transaction;
+        System.out.println("The result of getTransactionByHash is " + new Gson().toJson(transaction));
+
+        Header header = ckbService.getTipHeader().send().getHeader();
+        System.out.println("The tip header is " + new Gson().toJson(header));
+
+        BigInteger blockNumber = ckbService.getTipBlockNumber().send().getBlockNumber();
+        System.out.println("The result of getTipBlockNumber is " + blockNumber.toString());
+
+        String nodeId = ckbService.localNodeId().send().getNodeId();
+        System.out.println("Local node id is " + nodeId);
+
+        List<Cell> cells = ckbService.getCellsByTypeHash(
+                "0xcf7294651a9e2033243b04cfd3fa35097d56b811824691a75cd29d50ac23720a", 1, 100
+        ).send().getCells();
+        System.out.println("The result of getCellsByTypeHash is " + new Gson().toJson(cells));
+
+        Cell cell = ckbService.getCurrentCell(
+                new Cell.OutPoint("0x10262d4d6918774ae939a55b88f1b2c847f75489e36cd58cca03bad5c216db4f", 0)
+        ).send().getCell();
+        System.out.println("Current cell is " + new Gson().toJson(cell));
+
+        String hash = ckbService.sendTransaction(
+                new Transaction(0, Collections.emptyList(), Collections.emptyList(), Collections.emptyList())
+        ).send().getTransactionHash();
+        System.out.println("The result transaction hash of sending transaction is " + hash);
 
     }
 
