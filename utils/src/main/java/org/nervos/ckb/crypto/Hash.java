c37d8e82b1701f20f3e964d7f41a59365c1da359
duanyytop <duanyytop@gmail.com>
2019-03-06 11:08:55 +0800

    feat: Implement Blake2b Hashing

--- utils/src/main/java/org/nervos/ckb/crypto/Hash.java
+++ utils/src/main/java/org/nervos/ckb/crypto/Hash.java
@@ -1,6 +1,6 @@
 package org.nervos.ckb.crypto;
 
-import org.bouncycastle.jcajce.provider.digest.SHA3;
+import org.bouncycastle.crypto.digests.Blake2bDigest;
 import org.nervos.ckb.utils.Numeric;
 
 import java.nio.charset.StandardCharsets;
@@ -12,49 +12,51 @@ public class Hash {
     private Hash() { }
 
     /**
-     * SHA3-256 hash function.
+     * Blake2b-256 hash function.
      *
      * @param hexInput hex encoded input data with optional 0x prefix
      * @return hash value as hex encoded string
      */
-    public static String sha3(String hexInput) {
+    public static String blake2b(String hexInput) {
         byte[] bytes = Numeric.hexStringToByteArray(hexInput);
-        byte[] result = sha3(bytes);
+        byte[] result = blake2b(bytes);
         return Numeric.toHexString(result);
     }
 
     /**
-     * SHA3-256 hash function.
+     * Blake2b-256 hash function.
      *
      * @param input binary encoded input data
      * @param offset of start of data
      * @param length of data
      * @return hash value
      */
-    public static byte[] sha3(byte[] input, int offset, int length) {
-        SHA3.DigestSHA3 sha3 = new SHA3.Digest256();
-        sha3.update(input, offset, length);
-        return sha3.digest();
+    public static byte[] blake2b(byte[] input, int offset, int length) {
+        Blake2bDigest blake2b = new Blake2bDigest(256);
+        blake2b.update(input, offset, length);
+        byte[] out = new byte[32];
+        blake2b.doFinal(out, 0);
+        return out;
     }
 
 
     /**
-     * SHA3-256 hash function.
+     * Blake2b-256 hash function.
      *
      * @param input binary encoded input data
      * @return hash value
      */
-    public static byte[] sha3(byte[] input) {
-        return sha3(input, 0, input.length);
+    public static byte[] blake2b(byte[] input) {
+        return blake2b(input, 0, input.length);
     }
 
     /**
-     * SHA3-256 hash function that operates on a UTF-8 encoded String.
+     * Blake2b-256 hash function that operates on a UTF-8 encoded String.
      *
      * @param utf8String UTF-8 encoded string
      * @return hash value as hex encoded string
      */
-    public static String sha3String(String utf8String) {
-        return Numeric.toHexString(sha3(utf8String.getBytes(StandardCharsets.UTF_8)));
+    public static String blake2bString(String utf8String) {
+        return Numeric.toHexString(blake2b(utf8String.getBytes(StandardCharsets.UTF_8)));
     }
 }
