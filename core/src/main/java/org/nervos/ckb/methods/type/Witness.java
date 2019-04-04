d6eca649560bb43dfe9c5d573421fdf4b53f4ae9
秋月夜 <baojun.cz@gmail.com>
2019-04-04 10:46:40 +0800

    feat: remove index field from witness

--- core/src/main/java/org/nervos/ckb/methods/type/Witness.java
+++ core/src/main/java/org/nervos/ckb/methods/type/Witness.java
@@ -3,11 +3,9 @@ package org.nervos.ckb.methods.type;
 import java.util.List;
 
 public class Witness {
-    public int index;
     public List<String> data;
 
-    public Witness(int index, List<String> data) {
-        this.index = index;
+    public Witness(List<String> data) {
         this.data = data;
     }
 }
