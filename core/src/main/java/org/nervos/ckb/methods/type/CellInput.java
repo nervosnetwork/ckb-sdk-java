d4fa14202a08ddee97b06ca514694232849aa5aa
duanyytop <duanyytop@gmail.com>
2019-04-25 15:25:13 +0800

    chore: update CellInput data type

--- core/src/main/java/org/nervos/ckb/methods/type/CellInput.java
+++ core/src/main/java/org/nervos/ckb/methods/type/CellInput.java
@@ -6,20 +6,29 @@ import java.util.List;
 /** Created by duanyytop on 2019-01-08. Copyright © 2018 Nervos Foundation. All rights reserved. */
 public class CellInput {
 
-  @JsonProperty("tx_hash")
-  public String txHash;
+  @JsonProperty("previous_output")
+  public PreviousOutput previousOutput;
 
-  public long index;
-
-  public long since;
   public List<String> args;
 
   public CellInput() {}
 
-  public CellInput(String txHash, long index, long since, List<String> args) {
-    this.txHash = txHash;
-    this.index = index;
-    this.since = since;
+  public CellInput(PreviousOutput previousOutput, List<String> args) {
+    this.previousOutput = previousOutput;
     this.args = args;
+  }
+
+  public static class PreviousOutput {
+    @JsonProperty("tx_hash")
+    public String txHash;
+
+    public long index;
+
+    public PreviousOutput() {}
+
+    public PreviousOutput(String txHash, long index) {
+      this.txHash = txHash;
+      this.index = index;
+    }
   }
 }
