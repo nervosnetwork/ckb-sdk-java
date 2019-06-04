936734e81bc2fb0909029bc71bec88c4e1918922
duanyytop <duanyytop@gmail.com>
2019-06-04 11:37:12 +0800

    feat: update transaction sign method for witness

--- core/src/main/java/org/nervos/ckb/methods/type/Witness.java
+++ core/src/main/java/org/nervos/ckb/methods/type/Witness.java
@@ -3,12 +3,13 @@ package org.nervos.ckb.methods.type;
 import java.math.BigInteger;
 import java.util.ArrayList;
 import java.util.List;
+import org.nervos.ckb.crypto.Blake2b;
 import org.nervos.ckb.crypto.secp256k1.ECKeyPair;
 import org.nervos.ckb.crypto.secp256k1.Sign;
 import org.nervos.ckb.utils.Numeric;
 
 public class Witness {
-  public List<String> data;
+  public List<String> data = new ArrayList<>();
 
   public Witness() {}
 
@@ -17,14 +18,23 @@ public class Witness {
   }
 
   public Witness(BigInteger privateKey, String txHash) {
+    List<String> oldData = data;
+    Blake2b blake2b = new Blake2b();
+    blake2b.update(Numeric.hexStringToByteArray(txHash));
+    for (String datum : data) {
+      blake2b.update(Numeric.hexStringToByteArray(datum));
+    }
+    String message = blake2b.doFinalString();
+
     ECKeyPair ecKeyPair = ECKeyPair.createWithPrivateKey(privateKey, false);
-    byte[] signatureBytes =
-        Sign.signMessage(Numeric.hexStringToByteArray(txHash), ecKeyPair).getDerSignature();
-    String signature = Numeric.toHexString(signatureBytes);
+    String publicKey =
+        Numeric.toHexStringWithPrefixZeroPadded(Sign.publicKeyFromPrivate(privateKey, true), 66);
+    String signature =
+        Numeric.toHexString(
+            Sign.signMessage(Numeric.hexStringToByteArray(message), ecKeyPair).getDerSignature());
     data = new ArrayList<>();
-    data.add(
-        Numeric.toHexStringWithPrefixZeroPadded(Sign.publicKeyFromPrivate(privateKey, true), 66));
+    data.add(publicKey);
     data.add(signature);
-    data.add(Numeric.littleEndian(signatureBytes.length));
+    data.addAll(oldData);
   }
 }
