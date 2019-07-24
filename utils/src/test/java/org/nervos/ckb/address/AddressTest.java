cd93318d470d8437201b004a3834720f95b74861
duanyytop <duanyytop@gmail.com>
2019-07-24 09:10:01 +0800

    feat: Update address format generator

--- utils/src/test/java/org/nervos/ckb/address/AddressTest.java
+++ utils/src/test/java/org/nervos/ckb/address/AddressTest.java
@@ -38,7 +38,7 @@ public class AddressTest {
 
   @Test
   public void testArgToAddressTestnet() throws AddressFormatException {
-    String expected = "ckt1q9gry5zg95w42h05rnvm50g0x8c2rt9reu0zjkhltdzxlsz47hacdwv77jds9waehx";
+    String expected = "ckt1qyqz6824th6pekd6858nru9p4j3u783fttl4k3r0cp2lt7uxhx00fxcxpzeq8";
     String args = "0x2d1d555df41cd9ba3d0f31f0a1aca3cf1e295aff5b446fc055f5fb86b99ef49b";
     AddressUtils utils = new AddressUtils(Network.TESTNET);
     String actual = utils.generate(args);
@@ -49,7 +49,7 @@ public class AddressTest {
   public void testPublicKeyHashToAddressTestnet() {
     AddressUtils utils = new AddressUtils(Network.TESTNET);
     Assertions.assertEquals(
-        "ckt1q9gry5zgxmpjnmtrp4kww5r39frh2sm89tdt2l6v234ygf",
+        "ckt1qyqrdsefa43s6m882pcj53m4gdnj4k440axqswmu83",
         utils.generateFromPublicKey(
             "0x024a501efd328e062c8675f2365970728c859c592beeefd6be8ead3d901330bc01"));
   }
@@ -58,7 +58,7 @@ public class AddressTest {
   public void testPublicKeyHashToAddressMainnet() {
     AddressUtils utils = new AddressUtils(Network.MAINNET);
     Assertions.assertEquals(
-        "ckb1q9gry5zgxmpjnmtrp4kww5r39frh2sm89tdt2l6vqdd7em",
+        "ckb1qyqrdsefa43s6m882pcj53m4gdnj4k440axqdt9rtd",
         utils.generateFromPublicKey(
             "0x024a501efd328e062c8675f2365970728c859c592beeefd6be8ead3d901330bc01"));
   }
@@ -73,15 +73,14 @@ public class AddressTest {
                 true)
             .toString(16);
     Assertions.assertEquals(
-        "ckt1q9gry5zgxmpjnmtrp4kww5r39frh2sm89tdt2l6v234ygf",
-        utils.generateFromPublicKey(publicKey));
+        "ckt1qyqrdsefa43s6m882pcj53m4gdnj4k440axqswmu83", utils.generateFromPublicKey(publicKey));
   }
 
   @Test
   public void testBlake160FromAddressTestnet() {
     AddressUtils utils = new AddressUtils(Network.TESTNET);
     String blake160 =
-        utils.getBlake160FromAddress("ckt1q9gry5zgxmpjnmtrp4kww5r39frh2sm89tdt2l6v234ygf");
+        utils.getBlake160FromAddress("ckt1qyqrdsefa43s6m882pcj53m4gdnj4k440axqswmu83");
     Assertions.assertEquals(blake160, "0x36c329ed630d6ce750712a477543672adab57f4c");
   }
 
@@ -89,7 +88,15 @@ public class AddressTest {
   public void testBlake160FromAddressMainnet() {
     AddressUtils utils = new AddressUtils(Network.MAINNET);
     String blake160 =
-        utils.getBlake160FromAddress("ckb1q9gry5zgxmpjnmtrp4kww5r39frh2sm89tdt2l6vqdd7em");
+        utils.getBlake160FromAddress("ckb1qyqrdsefa43s6m882pcj53m4gdnj4k440axqdt9rtd");
     Assertions.assertEquals(blake160, "0x36c329ed630d6ce750712a477543672adab57f4c");
+  }
+
+  @Test
+  public void testPubkeyHashToAddressMainnetRFC() {
+    AddressUtils utils = new AddressUtils(Network.MAINNET);
+    Assertions.assertEquals(
+        "ckb1qyqp8eqad7ffy42ezmchkjyz54rhcqf8q9pqrn323p",
+        utils.generate("0x13e41d6F9292555916f17B4882a5477C01270142"));
   }
 }
