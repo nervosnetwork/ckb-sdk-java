0c7b02b3f4ca001653a2ab00b673c9393884b8a2
duanyytop <duanyytop@gmail.com>
2019-08-30 17:56:25 +0800

    feat: add serialization encoder

--- serialization/src/main/java/org/nervos/ckb/Encoder.java
+++ serialization/src/main/java/org/nervos/ckb/Encoder.java
@@ -1,7 +1,30 @@
 package org.nervos.ckb;
 
-/**
- * Copyright © 2019 Nervos Foundation. All rights reserved.
- */
+import java.util.ArrayList;
+import java.util.List;
+import org.nervos.ckb.methods.type.Script;
+import org.nervos.ckb.type.*;
+
+/** Copyright © 2019 Nervos Foundation. All rights reserved. */
 public class Encoder {
+
+  public static Table toTable(Object object) {
+    if (object instanceof Script) {
+      Script script = (Script) object;
+      List<Type> types = new ArrayList<>();
+      types.add(new Byte32(script.codeHash));
+      types.add(Script.DATA.equals(script.hashType) ? new Byte1("00") : new Byte1("01"));
+      List<Bytes> argList = new ArrayList<>();
+      for (String arg : script.args) {
+        argList.add(new Bytes(arg));
+      }
+      types.add(new BytesVec(argList));
+      return new Table(types);
+    }
+    return null;
+  }
+
+  public static byte[] encode(Table table) {
+    return table.toBytes();
+  }
 }
