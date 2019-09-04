efbe0eb83bdd68340693a6a64fa55d4486134420
duanyytop <duanyytop@gmail.com>
2019-09-04 16:49:54 +0800

    refactor: refactor serialization and utils

--- serialization/src/main/java/org/nervos/ckb/Encoder.java
+++ serialization/src/main/java/org/nervos/ckb/Encoder.java
@@ -1,6 +1,6 @@
 package org.nervos.ckb;
 
-import org.nervos.ckb.type.Type;
+import org.nervos.ckb.type.base.Type;
 
 /** Copyright © 2019 Nervos Foundation. All rights reserved. */
 public class Encoder {
