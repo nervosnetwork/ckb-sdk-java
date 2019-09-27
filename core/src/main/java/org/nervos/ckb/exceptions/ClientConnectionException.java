70dc985a9fce63806608778830bbcb31dc10add4
duanyytop <duanyytop@gmail.com>
2019-09-27 11:22:11 +0800

    refactor: remove no use exception

--- core/src/main/java/org/nervos/ckb/exceptions/ClientConnectionException.java
+++
@@ -1,8 +0,0 @@
-package org.nervos.ckb.exceptions;
-
-/** Client connection exception. */
-public class ClientConnectionException extends RuntimeException {
-  public ClientConnectionException(String message) {
-    super(message);
-  }
-}
