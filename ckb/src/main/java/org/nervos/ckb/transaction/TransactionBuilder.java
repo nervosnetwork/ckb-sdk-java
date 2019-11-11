e4276c234a77e733bc7bc5fa3da27e20ecdf284d
duanyytop <duanyytop@gmail.com>
2019-11-11 18:07:52 +0800

    feat: impl nervos dao deposit

--- ckb/src/main/java/org/nervos/ckb/transaction/TransactionBuilder.java
+++ ckb/src/main/java/org/nervos/ckb/transaction/TransactionBuilder.java
@@ -1,29 +1,27 @@
 package org.nervos.ckb.transaction;
 
-import java.io.IOException;
-import java.math.BigInteger;
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.List;
 import org.nervos.ckb.service.Api;
 import org.nervos.ckb.system.SystemContract;
-import org.nervos.ckb.system.type.SystemScriptCell;
 import org.nervos.ckb.type.cell.CellDep;
 import org.nervos.ckb.type.cell.CellInput;
 import org.nervos.ckb.type.cell.CellOutput;
 import org.nervos.ckb.type.transaction.Transaction;
 import org.nervos.ckb.utils.Numeric;
 
+import java.io.IOException;
+import java.math.BigInteger;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
 /** Copyright © 2019 Nervos Foundation. All rights reserved. */
 public class TransactionBuilder {
 
-  private SystemScriptCell systemSecpCell;
-  private SystemScriptCell systemMultiSigCell;
   private List<CellInput> cellInputs = new ArrayList<>();
   private List<CellOutput> cellOutputs = new ArrayList<>();
   private List<String> cellOutputsData = new ArrayList<>();
+  private List<CellDep> cellDeps = new ArrayList<>();
   private List witnesses = new ArrayList<>();
-  private boolean isMultiSig = false;
 
   public TransactionBuilder(Api api) {
     this(api, false);
@@ -31,11 +29,12 @@ public class TransactionBuilder {
 
   public TransactionBuilder(Api api, boolean isMultiSig) {
     try {
-      this.isMultiSig = isMultiSig;
       if (isMultiSig) {
-        this.systemMultiSigCell = SystemContract.getSystemMultiSigCell(api);
+        this.cellDeps.add(
+            new CellDep(SystemContract.getSystemMultiSigCell(api).outPoint, CellDep.DEP_GROUP));
       } else {
-        this.systemSecpCell = SystemContract.getSystemSecpCell(api);
+        this.cellDeps.add(
+            new CellDep(SystemContract.getSystemSecpCell(api).outPoint, CellDep.DEP_GROUP));
       }
     } catch (Exception e) {
       e.printStackTrace();
@@ -66,6 +65,10 @@ public class TransactionBuilder {
     cellOutputs.addAll(outputs);
   }
 
+  public void addCellDep(CellDep cellDep) {
+    cellDeps.add(cellDep);
+  }
+
   public Transaction buildTx() throws IOException {
     BigInteger needCapacity = BigInteger.ZERO;
     for (CellOutput output : cellOutputs) {
@@ -78,12 +81,6 @@ public class TransactionBuilder {
       cellOutputsData.add("0x");
     }
 
-    List<CellDep> cellDeps = new ArrayList<>();
-    if (isMultiSig) {
-      cellDeps.add(new CellDep(systemMultiSigCell.outPoint, CellDep.DEP_GROUP));
-    } else {
-      cellDeps.add(new CellDep(systemSecpCell.outPoint, CellDep.DEP_GROUP));
-    }
     return new Transaction(
         "0",
         cellDeps,
--- example/src/main/java/org/nervos/ckb/NervosDaoExample.java
+++ example/src/main/java/org/nervos/ckb/NervosDaoExample.java
@@ -2,10 +2,18 @@ package org.nervos.ckb;
 
 import java.io.IOException;
 import java.math.BigInteger;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+import org.nervos.ckb.crypto.secp256k1.Sign;
 import org.nervos.ckb.service.Api;
 import org.nervos.ckb.system.SystemContract;
-import org.nervos.ckb.transaction.LockUtils;
+import org.nervos.ckb.transaction.*;
+import org.nervos.ckb.type.OutPoint;
 import org.nervos.ckb.type.Script;
+import org.nervos.ckb.type.Witness;
+import org.nervos.ckb.type.cell.CellDep;
 import org.nervos.ckb.type.cell.CellOutput;
 
 /** Copyright © 2019 Nervos Foundation. All rights reserved. */
@@ -24,12 +32,50 @@ public class NervosDaoExample {
     api = new Api(NODE_URL, false);
   }
 
-  private static void depositToDao(BigInteger capacity, BigInteger fee) throws IOException {
+  private static OutPoint depositToDao(BigInteger capacity) throws IOException {
     Script lock =
         LockUtils.generateLockScriptWithAddress(
             MinerAddress, SystemContract.getSystemSecpCell(api).cellHash);
     Script type =
         new Script(SystemContract.getSystemNervosDaoCell(api).cellHash, "0x", Script.TYPE);
     CellOutput cellOutput = new CellOutput(capacity.toString(16), lock, type);
+    CellOutput changeOutput = new CellOutput("0x0", lock);
+    List<CellOutput> cellOutputs = Arrays.asList(cellOutput, changeOutput);
+
+    List<ScriptGroupWithPrivateKeys> scriptGroupWithPrivateKeysList = new ArrayList<>();
+    TransactionBuilder txBuilder = new TransactionBuilder(api);
+    txBuilder.addOutputs(cellOutputs);
+    txBuilder.addCellDep(
+        new CellDep(SystemContract.getSystemNervosDaoCell(api).outPoint, CellDep.DEP_GROUP));
+
+    // You can get fee rate by rpc or set a simple number
+    // BigInteger feeRate = Numeric.toBigInt(api.estimateFeeRate("5").feeRate);
+    BigInteger feeRate = BigInteger.valueOf(1024);
+    CollectUtils collectUtils = new CollectUtils(api);
+    List<CellsWithAddress> cellsWithAddresses =
+        collectUtils.collectInputs(
+            Collections.singletonList(MinerAddress), cellOutputs, feeRate, Sign.SIGN_LENGTH * 2);
+
+    int startIndex = 0;
+    for (CellsWithAddress cellsWithAddress : cellsWithAddresses) {
+      txBuilder.addInputs(cellsWithAddress.inputs);
+      for (int i = 0; i < cellsWithAddress.inputs.size(); i++) {
+        txBuilder.addWitness(i == 0 ? new Witness(Witness.SIGNATURE_PLACEHOLDER) : "0x");
+      }
+      scriptGroupWithPrivateKeysList.add(
+          new ScriptGroupWithPrivateKeys(
+              new ScriptGroup(NumberUtils.regionToList(startIndex, cellsWithAddress.inputs.size())),
+              Collections.singletonList(MinerPrivateKey)));
+      startIndex += cellsWithAddress.inputs.size();
+    }
+
+    Secp256k1SighashAllBuilder signBuilder = new Secp256k1SighashAllBuilder(txBuilder.buildTx());
+
+    for (ScriptGroupWithPrivateKeys scriptGroupWithPrivateKeys : scriptGroupWithPrivateKeysList) {
+      signBuilder.sign(
+          scriptGroupWithPrivateKeys.scriptGroup, scriptGroupWithPrivateKeys.privateKeys.get(0));
+    }
+    String txHash = api.sendTransaction(signBuilder.buildTx());
+    return new OutPoint(txHash, "0x0");
   }
 }
