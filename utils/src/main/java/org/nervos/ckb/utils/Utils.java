53c0492637167374be1313e6f99760da9851449f
duanyytop <duanyytop@gmail.com>
2019-11-12 22:16:42 +0800

    refactor: remove shannonToCkb method

--- utils/src/main/java/org/nervos/ckb/utils/Utils.java
+++ utils/src/main/java/org/nervos/ckb/utils/Utils.java
@@ -5,14 +5,6 @@ import java.math.BigInteger;
 /** Copyright © 2019 Nervos Foundation. All rights reserved. */
 public class Utils {
 
-  public static BigInteger shannonToCkb(BigInteger value) {
-    return value.divide(BigInteger.TEN.pow(8));
-  }
-
-  public static BigInteger shannonToCkb(long value) {
-    return BigInteger.valueOf(value).divide(BigInteger.TEN.pow(8));
-  }
-
   public static BigInteger ckbToShannon(BigInteger value) {
     return value.multiply(BigInteger.TEN.pow(8));
   }
--- utils/src/test/java/org/nervos/ckb/utils/UtilsTest.java
+++ utils/src/test/java/org/nervos/ckb/utils/UtilsTest.java
@@ -8,18 +8,6 @@ import org.junit.jupiter.api.Test;
 public class UtilsTest {
 
   @Test
-  public void testShannonToCkb() {
-    Assertions.assertEquals(BigInteger.valueOf(234), Utils.shannonToCkb(23438000000L));
-  }
-
-  @Test
-  public void testShannonToCkbWithBigInteger() {
-    Assertions.assertEquals(
-        BigInteger.valueOf(265893580000L),
-        Utils.shannonToCkb(new BigInteger("26589358000000000000")));
-  }
-
-  @Test
   public void testCkbToShannon() {
     Assertions.assertEquals(BigInteger.valueOf(234300000000L), Utils.ckbToShannon(2343));
   }
