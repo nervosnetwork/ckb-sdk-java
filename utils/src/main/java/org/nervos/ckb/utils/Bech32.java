8c209ef9bc1e2ec9b2aeea50d8dfe9d2a90a2350
duanyytop <duanyytop@gmail.com>
2019-11-29 08:48:16 +0800

    chore: add apache license

--- utils/src/main/java/org/nervos/ckb/utils/Bech32.java
+++ utils/src/main/java/org/nervos/ckb/utils/Bech32.java
@@ -20,6 +20,21 @@ import java.util.Arrays;
 import java.util.Locale;
 import org.nervos.ckb.exceptions.AddressFormatException;
 
+/*
+ * Copyright 2018 Coinomi Ltd
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 public class Bech32 {
   /** The Bech32 character set for encoding. */
   private static final String CHARSET = "qpzry9x8gf2tvdw0s3jn54khce6mua7l";
