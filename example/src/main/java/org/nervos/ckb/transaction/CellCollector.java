557632aef474d7ff310a83e292f117b0502dbdba
duanyytop <duanyytop@gmail.com>
2019-12-10 16:33:29 +0800

    chore: remove useless code

--- example/src/main/java/org/nervos/ckb/transaction/CellCollector.java
+++ example/src/main/java/org/nervos/ckb/transaction/CellCollector.java
@@ -4,10 +4,11 @@ import java.io.IOException;
 import java.math.BigInteger;
 import java.util.*;
 import org.nervos.ckb.service.Api;
-import org.nervos.ckb.system.SystemContract;
-import org.nervos.ckb.system.type.SystemScriptCell;
 import org.nervos.ckb.type.Witness;
-import org.nervos.ckb.type.cell.*;
+import org.nervos.ckb.type.cell.CellInput;
+import org.nervos.ckb.type.cell.CellOutput;
+import org.nervos.ckb.type.cell.CellOutputWithOutPoint;
+import org.nervos.ckb.type.cell.CellWithStatus;
 import org.nervos.ckb.type.transaction.Transaction;
 import org.nervos.ckb.utils.Calculator;
 import org.nervos.ckb.utils.Numeric;
@@ -30,142 +31,6 @@ public class CellCollector {
   public CellCollector(Api api) {
     this.api = api;
     this.skipDataAndType = true;
-  }
-
-  public CollectResult collectInputs(
-      List<String> addresses,
-      List<CellOutput> cellOutputs,
-      BigInteger feeRate,
-      int initialLength,
-      List<CellDep> cellDeps,
-      List<String> outputsData,
-      List<String> headerDeps)
-      throws IOException {
-
-    List<String> lockHashes = new ArrayList<>();
-    for (String address : addresses) {
-      AddressParseResult addressParseResult = AddressParser.parse(address);
-      lockHashes.add(addressParseResult.script.computeHash());
-    }
-    List<String> cellOutputsData = new ArrayList<>();
-    for (int i = 0; i < cellOutputs.size() - 1; i++) {
-      BigInteger size = cellOutputs.get(i).occupiedCapacity("0x");
-      if (size.compareTo(Numeric.toBigInt(cellOutputs.get(i).capacity)) > 0) {
-        throw new IOException("Cell output byte size must not be bigger than capacity");
-      }
-      cellOutputsData.add("0x");
-    }
-    SystemScriptCell systemScriptCell = SystemContract.getSystemSecpCell(api);
-    cellOutputsData.add("0x");
-
-    if (outputsData != null && outputsData.size() > 0) {
-      cellOutputsData = outputsData;
-    }
-
-    List<CellDep> cellDepList =
-        Collections.singletonList(new CellDep(systemScriptCell.outPoint, CellDep.DEP_GROUP));
-    if (cellDeps != null && cellDeps.size() > 0) {
-      cellDepList = cellDeps;
-    }
-
-    List<String> headerDepList = Collections.emptyList();
-    if (headerDeps != null && headerDeps.size() > 0) {
-      headerDepList = headerDeps;
-    }
-
-    Transaction transaction =
-        new Transaction(
-            "0",
-            cellDepList,
-            headerDepList,
-            Collections.emptyList(),
-            cellOutputs,
-            cellOutputsData,
-            Collections.emptyList());
-
-    BigInteger inputsCapacity = BigInteger.ZERO;
-    List<CellInput> cellInputs = new ArrayList<>();
-    Map<String, List<CellInput>> lockInputsMap = new HashMap<>();
-    for (String lockHash : lockHashes) {
-      lockInputsMap.put(lockHash, new ArrayList<>());
-    }
-    List witnesses = new ArrayList<>();
-
-    CellOutput changeOutput = cellOutputs.get(cellOutputs.size() - 1);
-
-    BigInteger needCapacity = BigInteger.ZERO;
-    for (CellOutput cellOutput : cellOutputs) {
-      needCapacity = needCapacity.add(Numeric.toBigInt(cellOutput.capacity));
-    }
-    List<CellOutputWithOutPoint> cellOutputList;
-    for (int index = 0; index < lockHashes.size(); index++) {
-      long toBlockNumber = api.getTipBlockNumber().longValue();
-      long fromBlockNumber = 1;
-
-      while (fromBlockNumber <= toBlockNumber
-          && inputsCapacity.compareTo(needCapacity.add(calculateTxFee(transaction, feeRate))) < 0) {
-        long currentToBlockNumber = Math.min(fromBlockNumber + 100, toBlockNumber);
-        cellOutputList =
-            api.getCellsByLockHash(
-                lockHashes.get(index),
-                BigInteger.valueOf(fromBlockNumber).toString(),
-                BigInteger.valueOf(currentToBlockNumber).toString());
-        for (CellOutputWithOutPoint cellOutputWithOutPoint : cellOutputList) {
-          if (skipDataAndType) {
-            CellWithStatus cellWithStatus = api.getLiveCell(cellOutputWithOutPoint.outPoint, true);
-            String outputsDataContent = cellWithStatus.cell.data.content;
-            CellOutput cellOutput = cellWithStatus.cell.output;
-            if ((!Strings.isEmpty(outputsDataContent) && !"0x".equals(outputsDataContent))
-                || cellOutput.type != null) {
-              continue;
-            }
-          }
-          CellInput cellInput = new CellInput(cellOutputWithOutPoint.outPoint, "0x0");
-          inputsCapacity = inputsCapacity.add(Numeric.toBigInt(cellOutputWithOutPoint.capacity));
-          List<CellInput> cellInputList = lockInputsMap.get(lockHashes.get(index));
-          cellInputList.add(cellInput);
-          cellInputs.add(cellInput);
-          witnesses.add("0x");
-          transaction.inputs = cellInputs;
-          transaction.witnesses = witnesses;
-          BigInteger sumNeedCapacity =
-              needCapacity
-                  .add(calculateTxFee(transaction, feeRate))
-                  .add(calculateOutputSize(changeOutput));
-          if (inputsCapacity.compareTo(sumNeedCapacity) > 0) {
-            // update witness of group first element
-            int witnessIndex = 0;
-            for (String lockHash : lockHashes) {
-              if (lockInputsMap.get(lockHash).size() == 0) break;
-              witnesses.set(witnessIndex, new Witness(NumberUtils.getZeros(initialLength)));
-              witnessIndex += lockInputsMap.get(lockHash).size();
-            }
-            transaction.witnesses = witnesses;
-            // calculate sum need capacity again
-            sumNeedCapacity =
-                needCapacity
-                    .add(calculateTxFee(transaction, feeRate))
-                    .add(calculateOutputSize(changeOutput));
-            if (inputsCapacity.compareTo(sumNeedCapacity) > 0) {
-              break;
-            }
-          }
-        }
-        fromBlockNumber = currentToBlockNumber + 1;
-      }
-    }
-    if (inputsCapacity.compareTo(needCapacity.add(calculateTxFee(transaction, feeRate))) < 0) {
-      throw new IOException("Capacity not enough!");
-    }
-    BigInteger changeCapacity =
-        inputsCapacity.subtract(needCapacity.add(calculateTxFee(transaction, feeRate)));
-    List<CellsWithAddress> cellsWithAddresses = new ArrayList<>();
-    for (Map.Entry<String, List<CellInput>> entry : lockInputsMap.entrySet()) {
-      cellsWithAddresses.add(
-          new CellsWithAddress(
-              entry.getValue(), addresses.get(lockHashes.indexOf(entry.getKey()))));
-    }
-    return new CollectResult(cellsWithAddresses, Numeric.toHexStringWithPrefix(changeCapacity));
   }
 
   public CollectResult collectInputs(
