afa534ae025dce16e53412f2675cda1297cbf2db
duanyytop <duanyytop@gmail.com>
2020-03-02 15:34:53 +0800

    chore: add sudt example

---
+++ example/src/main/java/org/nervos/ckb/SUDTExample.java
@@ -0,0 +1,17 @@
+package org.nervos.ckb;
+
+/** Copyright © 2020 Nervos Foundation. All rights reserved. */
+public class SUDTExample {
+
+  public static void main(String[] args) {
+    System.out.println("sUDT example");
+  }
+
+  private static void issue() {}
+
+  private static void mint() {}
+
+  private static void transfer() {}
+
+  private static void burn() {}
+}
