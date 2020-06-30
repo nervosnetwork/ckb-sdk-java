caa9654bc91a412453bbe6ac10d54ca004752eec
duanyytop <duanyytop@gmail.com>
2020-06-30 16:43:01 +0800

    refactor: Remove uesless code of utils

--- utils/src/main/java/org/nervos/ckb/utils/Strings.java
+++ utils/src/main/java/org/nervos/ckb/utils/Strings.java
@@ -1,7 +1,5 @@
 package org.nervos.ckb.utils;
 
-import java.util.Collections;
-
 /** Copyright © 2018 Nervos Foundation. All rights reserved. */
 public class Strings {
   public static String zeros(int n) {
@@ -14,30 +12,5 @@ public class Strings {
 
   public static boolean isEmpty(String s) {
     return s == null || s.length() == 0;
-  }
-
-  public static byte[] asciiToHex(String asciiValue, int length) {
-    char[] chars = asciiValue.toCharArray();
-    StringBuilder hex = new StringBuilder();
-    for (int i = 0; i < chars.length; i++) {
-      hex.append(Integer.toHexString((int) chars[i]));
-    }
-
-    String hexStr =
-        hex.toString() + "".join("", Collections.nCopies(length - (hex.length() / 2), "00"));
-    return Numeric.hexStringToByteArray(hexStr);
-  }
-
-  public static String hexStringToAscii(String hexStr) {
-    assert (hexStr.length() % 2 == 0);
-    StringBuilder asciiStr = new StringBuilder();
-    for (int i = 0; i < hexStr.length(); i += 2) {
-      String str = hexStr.substring(i, i + 2);
-      if (str.equals("00")) {
-        break;
-      }
-      asciiStr.append((char) Integer.parseInt(str, 16));
-    }
-    return asciiStr.toString();
   }
 }
