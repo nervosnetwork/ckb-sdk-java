fdb0bd92701d7591e8cd118768f22856de06df45
shaojunda <shaojunda@gmail.com>
2020-08-26 13:24:03 +0800

    chore: reuse rpc services

--- example/src/main/java/org/nervos/ckb/service/RpcCallback.java
+++
@@ -1,9 +0,0 @@
-package org.nervos.ckb.service;
-
-/** Copyright © 2019 Nervos Foundation. All rights reserved. */
-interface RpcCallback<T> {
-
-  void onFailure(String errorMessage);
-
-  void onResponse(T response);
-}
