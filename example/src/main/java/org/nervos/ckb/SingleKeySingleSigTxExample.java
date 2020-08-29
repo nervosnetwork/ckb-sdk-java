dd1727373ff558c68728cd940a35557ce670d374
shaojunda <shaojunda@gmail.com>
2020-08-29 12:32:50 +0800

    chore: update single sign examples

--- example/src/main/java/org/nervos/ckb/SingleKeySingleSigTxExample.java
+++ example/src/main/java/org/nervos/ckb/SingleKeySingleSigTxExample.java
@@ -14,6 +14,7 @@ import org.nervos.ckb.service.Api;
 import org.nervos.ckb.transaction.*;
 import org.nervos.ckb.type.Witness;
 import org.nervos.ckb.type.cell.CellOutput;
+import org.nervos.ckb.utils.Numeric;
 import org.nervos.ckb.utils.Utils;
 
 /** Copyright © 2019 Nervos Foundation. All rights reserved. */
@@ -99,9 +100,11 @@ public class SingleKeySingleSigTxExample {
             feeRate,
             Sign.SIGN_LENGTH * 2);
 
-    // update change output capacity after collecting cells
-    cellOutputs.get(cellOutputs.size() - 1).capacity = collectResult.changeCapacity;
-    txBuilder.setOutputs(cellOutputs);
+    // update change cell output capacity after collecting cells if there is changeOutput
+    if (Numeric.toBigInt(collectResult.changeCapacity).compareTo(BigInteger.ZERO) > 0) {
+      cellOutputs.get(cellOutputs.size() - 1).capacity = collectResult.changeCapacity;
+      txBuilder.setOutputs(cellOutputs);
+    }
 
     int startIndex = 0;
     for (CellsWithAddress cellsWithAddress : collectResult.cellsWithAddresses) {
--- example/src/main/java/org/nervos/ckb/SingleSigWithCkbIndexerTxExample.java
+++ example/src/main/java/org/nervos/ckb/SingleSigWithCkbIndexerTxExample.java
@@ -16,6 +16,7 @@ import org.nervos.ckb.transaction.*;
 import org.nervos.ckb.type.Witness;
 import org.nervos.ckb.type.cell.CellOutput;
 import org.nervos.ckb.type.transaction.Transaction;
+import org.nervos.ckb.utils.Numeric;
 import org.nervos.ckb.utils.Utils;
 
 /** Copyright © 2019 Nervos Foundation. All rights reserved. */
@@ -105,9 +106,11 @@ public class SingleSigWithCkbIndexerTxExample {
         txUtils.collectInputsWithCkbIndexer(
             SendAddresses, txBuilder.buildTx(), feeRate, Sign.SIGN_LENGTH * 2, true);
 
-    // update change cell output capacity after collecting cells
-    cellOutputs.get(cellOutputs.size() - 1).capacity = collectResult.changeCapacity;
-    txBuilder.setOutputs(cellOutputs);
+    // update change cell output capacity after collecting cells if there is changeOutput
+    if (Numeric.toBigInt(collectResult.changeCapacity).compareTo(BigInteger.ZERO) > 0) {
+      cellOutputs.get(cellOutputs.size() - 1).capacity = collectResult.changeCapacity;
+      txBuilder.setOutputs(cellOutputs);
+    }
 
     int startIndex = 0;
     for (CellsWithAddress cellsWithAddress : collectResult.cellsWithAddresses) {
