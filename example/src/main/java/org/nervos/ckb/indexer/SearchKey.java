8ad406b451592c65407d7703e66a3f288b497e55
duanyytop <duanyytop@gmail.com>
2021-01-20 18:05:59 +0800

    feat: Add acp transaction example

--- example/src/main/java/org/nervos/ckb/indexer/SearchKey.java
+++ example/src/main/java/org/nervos/ckb/indexer/SearchKey.java
@@ -9,13 +9,31 @@ public class SearchKey {
   @SerializedName("script_type")
   public String scriptType;
 
+  public Filter filter;
+
+  public SearchKey(Script script, String scriptType, Filter filter) {
+    this.script = script;
+    this.scriptType = scriptType;
+    this.filter = filter;
+  }
+
   public SearchKey(Script script, String scriptType) {
     this.script = script;
     this.scriptType = scriptType;
+    this.filter = null;
   }
 
   public SearchKey(Script script) {
     this.script = script;
     this.scriptType = "lock";
+    this.filter = null;
+  }
+
+  public static class Filter {
+    public Script script;
+
+    public Filter(Script script) {
+      this.script = script;
+    }
   }
 }
--- utils/src/main/java/org/nervos/ckb/address/AddressUtils.java
+++ utils/src/main/java/org/nervos/ckb/address/AddressUtils.java
@@ -81,7 +81,6 @@ public class AddressUtils {
   public static String parse(String address) throws AddressFormatException {
     String payload = parsePrefix(address);
     String prefixCodeHash = payload.substring(TYPE.length());
-    System.out.println(payload);
     if (prefixCodeHash.startsWith(CODE_HASH_IDX_BLAKE160)) {
       return payload.substring((TYPE + CODE_HASH_IDX_BLAKE160).length());
     }
