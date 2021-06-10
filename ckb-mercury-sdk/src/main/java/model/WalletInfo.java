cadb743a3ef59355cd872c54a1ccd8a299876180
zhengjianhui <815168733@qq.com>
2021-06-10 10:20:49 +0800

    refactor: Remove min_udt and min_ckb

--- ckb-mercury-sdk/src/main/java/model/WalletInfo.java
+++ ckb-mercury-sdk/src/main/java/model/WalletInfo.java
@@ -7,14 +7,7 @@ public class WalletInfo {
   @SerializedName("udt_hash")
   public String udtHash;
 
-  @SerializedName("min_ckb")
-  public Integer minCkb = 3;
-
-  @SerializedName("min_udt")
-  public Integer minUdt;
-
-  public WalletInfo(String udtHash, Integer minUdt) {
+  public WalletInfo(String udtHash) {
     this.udtHash = udtHash;
-    this.minUdt = minUdt;
   }
 }
