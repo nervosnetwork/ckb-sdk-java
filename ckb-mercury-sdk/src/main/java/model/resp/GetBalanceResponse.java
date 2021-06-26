28b91383e3c1c3010a5609cd75f1d47ccf9ddc70
zhengjianhui <815168733@qq.com>
2021-06-26 10:40:44 +0800

    feat: CKB 0.42.0 support

--- ckb-mercury-sdk/src/main/java/model/resp/GetBalanceResponse.java
+++ ckb-mercury-sdk/src/main/java/model/resp/GetBalanceResponse.java
@@ -1,15 +1,10 @@
 package model.resp;
 
-import com.google.gson.annotations.SerializedName;
-
 public class GetBalanceResponse {
 
-  @SerializedName("owned")
   public String unconstrained;
 
-  @SerializedName("claimable")
   public String fleeting;
 
-  @SerializedName("locked")
   public String locked;
 }
