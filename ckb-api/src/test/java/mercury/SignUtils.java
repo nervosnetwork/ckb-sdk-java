d761b1cbbedaa07dbeec943fb7d4b2da1e90a1e4
zhengjianhui <815168733@qq.com>
2021-09-17 00:09:24 +0800

    feat(mercury-rpc): Adjusting the build_adjust_account_transaction interface in v0.2.0

---
+++ ckb-api/src/test/java/mercury/SignUtils.java
@@ -0,0 +1,37 @@
+package mercury;
+
+import constant.AddressWithKeyHolder;
+import java.io.IOException;
+import java.util.List;
+import org.nervos.ckb.transaction.Secp256k1SighashAllBuilder;
+import org.nervos.ckb.type.transaction.Transaction;
+import org.nervos.mercury.model.resp.MercuryScriptGroup;
+import org.nervos.mercury.model.resp.TransactionCompletionResponse;
+
+/** @author zjh @Created Date: 2021/7/23 @Description: @Modify by: */
+public class SignUtils {
+  public static Transaction sign(TransactionCompletionResponse s) throws IOException {
+    List<MercuryScriptGroup> scriptGroups = s.getScriptGroup();
+    Secp256k1SighashAllBuilder signBuilder = new Secp256k1SighashAllBuilder(s.txView);
+
+    for (MercuryScriptGroup sg : scriptGroups) {
+      signBuilder.sign(sg, AddressWithKeyHolder.getKey(sg.pubKey));
+    }
+
+    Transaction tx = signBuilder.buildTx();
+    return tx;
+  }
+
+  public static Transaction signByKey(TransactionCompletionResponse s, String key)
+      throws IOException {
+    List<MercuryScriptGroup> scriptGroups = s.getScriptGroup();
+    Secp256k1SighashAllBuilder signBuilder = new Secp256k1SighashAllBuilder(s.txView);
+
+    for (MercuryScriptGroup sg : scriptGroups) {
+      signBuilder.sign(sg, key);
+    }
+
+    Transaction tx = signBuilder.buildTx();
+    return tx;
+  }
+}
