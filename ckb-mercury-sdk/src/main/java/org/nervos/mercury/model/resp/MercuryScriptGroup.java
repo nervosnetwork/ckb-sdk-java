96ad1af5420c7143e43a62a0fe24dcabc4f1f144
“zhengjianhui” <815168733@qq.com>
2021-10-31 13:56:43 +0800

    feat(mercury-rpc): Signature of a witness built with mercury

--- ckb-mercury-sdk/src/main/java/org/nervos/mercury/model/resp/MercuryScriptGroup.java
+++ ckb-mercury-sdk/src/main/java/org/nervos/mercury/model/resp/MercuryScriptGroup.java
@@ -1,14 +1,47 @@
 package org.nervos.mercury.model.resp;
 
+import java.util.ArrayList;
 import java.util.List;
-import org.nervos.ckb.transaction.ScriptGroup;
+import org.nervos.ckb.type.transaction.Transaction;
+import org.nervos.mercury.model.resp.signature.SignatureAction;
 
-public class MercuryScriptGroup extends ScriptGroup {
+public class MercuryScriptGroup {
 
-  public String pubKey;
+  public SignatureAction action;
 
-  public MercuryScriptGroup(String pubKey, List<Integer> inputIndexes) {
-    super(inputIndexes);
-    this.pubKey = pubKey;
+  public Transaction transaction;
+
+  private String originalwitness;
+
+  public MercuryScriptGroup(SignatureAction action, Transaction transaction) {
+    this.action = action;
+    this.transaction = transaction;
+    this.originalwitness =
+        this.transaction.witnesses.get(this.action.signatureLocation.index).toString();
+  }
+
+  public Integer getOffset() {
+    return this.action.signatureLocation.offset;
+  }
+
+  public String getWitness() {
+    return originalwitness;
+  }
+
+  public Integer getWitnessIndex() {
+    return this.action.signatureLocation.index;
+  }
+
+  public String getAddress() {
+    return this.action.signatureInfo.address;
+  }
+
+  public List<String> getGroupWitnesses() {
+    List<String> groupWitnesses = new ArrayList<>(this.action.otherIndexesInGroup.size() + 1);
+    groupWitnesses.add(originalwitness);
+    for (Integer index : this.action.otherIndexesInGroup) {
+      groupWitnesses.add(this.transaction.witnesses.get(index).toString());
+    }
+    return groupWitnesses;
   }
 }
