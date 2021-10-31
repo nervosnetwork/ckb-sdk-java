3a653b775ad1354f7984cfaec357144b874577b3
zhengjianhui <815168733@qq.com>
2021-10-31 14:06:42 +0800

    feat(mercury-rpc): Signature of a witness built with mercury (#482)

--- ckb-api/src/test/java/utils/SignUtils.java
+++ ckb-api/src/test/java/utils/SignUtils.java
@@ -1,31 +1,29 @@
 package utils;
 
 import constant.AddressWithKeyHolder;
-import java.io.IOException;
 import java.util.List;
-import org.nervos.ckb.transaction.Secp256k1SighashAllBuilder;
 import org.nervos.ckb.type.transaction.Transaction;
 import org.nervos.mercury.model.resp.MercuryScriptGroup;
 import org.nervos.mercury.model.resp.TransactionCompletionResponse;
+import org.nervos.mercury.signature.Secp256k1SighashBuilder;
 
 /** @author zjh @Created Date: 2021/7/23 @Description: @Modify by: */
 public class SignUtils {
-  public static Transaction sign(TransactionCompletionResponse s) throws IOException {
+  public static Transaction sign(TransactionCompletionResponse s) {
     List<MercuryScriptGroup> scriptGroups = s.getScriptGroup();
-    Secp256k1SighashAllBuilder signBuilder = new Secp256k1SighashAllBuilder(s.txView);
+    Secp256k1SighashBuilder signBuilder = new Secp256k1SighashBuilder(s.txView);
 
     for (MercuryScriptGroup sg : scriptGroups) {
-      signBuilder.sign(sg, AddressWithKeyHolder.getKey(sg.pubKey));
+      signBuilder.sign(sg, AddressWithKeyHolder.getKey(sg.getAddress()));
     }
 
     Transaction tx = signBuilder.buildTx();
     return tx;
   }
 
-  public static Transaction signByKey(TransactionCompletionResponse s, String key)
-      throws IOException {
+  public static Transaction signByKey(TransactionCompletionResponse s, String key) {
     List<MercuryScriptGroup> scriptGroups = s.getScriptGroup();
-    Secp256k1SighashAllBuilder signBuilder = new Secp256k1SighashAllBuilder(s.txView);
+    Secp256k1SighashBuilder signBuilder = new Secp256k1SighashBuilder(s.txView);
 
     for (MercuryScriptGroup sg : scriptGroups) {
       signBuilder.sign(sg, key);
--- ckb-mercury-sdk/src/main/java/org/nervos/mercury/model/resp/TransactionCompletionResponse.java
+++ ckb-mercury-sdk/src/main/java/org/nervos/mercury/model/resp/TransactionCompletionResponse.java
@@ -1,10 +1,9 @@
 package org.nervos.mercury.model.resp;
 
+import static java.util.stream.Collectors.toList;
+
 import com.google.gson.annotations.SerializedName;
-import java.util.ArrayList;
 import java.util.List;
-import org.jetbrains.annotations.NotNull;
-import org.nervos.ckb.type.Witness;
 import org.nervos.ckb.type.transaction.Transaction;
 import org.nervos.mercury.model.resp.signature.SignatureAction;
 
@@ -17,34 +16,9 @@ public class TransactionCompletionResponse {
   public List<SignatureAction> signatureActions;
 
   public List<MercuryScriptGroup> getScriptGroup() {
-    List<MercuryScriptGroup> scriptGroups = new ArrayList<>(this.signatureActions.size());
-    for (int i = 0; i < this.signatureActions.size(); i++) {
-      SignatureAction signatureAction = this.signatureActions.get(i);
-      scriptGroups.add(
-          new MercuryScriptGroup(
-              signatureAction.signatureInfo.address, this.getWitnessGroupIndexes(signatureAction)));
-
-      this.txView.witnesses.set(
-          signatureAction.signatureLocation.index, converWitness(signatureAction));
-    }
-
-    return scriptGroups;
-  }
-
-  @NotNull
-  private Witness converWitness(SignatureAction signatureAction) {
-    return new Witness(
-        this.txView.witnesses.get(signatureAction.signatureLocation.index).toString());
-  }
-
-  @NotNull
-  private List<Integer> getWitnessGroupIndexes(SignatureAction signatureAction) {
-    List<Integer> witnessGroupIndexes =
-        new ArrayList<>(signatureAction.otherIndexesInGroup.size() + 1);
-    witnessGroupIndexes.add(signatureAction.signatureLocation.index);
-    for (Integer index : signatureAction.otherIndexesInGroup) {
-      witnessGroupIndexes.add(index);
-    }
-    return witnessGroupIndexes;
+    return this.signatureActions
+        .stream()
+        .map(x -> new MercuryScriptGroup(x, this.txView))
+        .collect(toList());
   }
 }
