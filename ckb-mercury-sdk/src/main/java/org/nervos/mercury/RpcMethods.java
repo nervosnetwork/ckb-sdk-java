ff4d74bab22bf1f7262dff41782e01325b45474e
“zhengjianhui” <815168733@qq.com>
2021-11-17 23:36:39 +0800

    feat(mercury): Rename register address

--- ckb-mercury-sdk/src/main/java/org/nervos/mercury/RpcMethods.java
+++ ckb-mercury-sdk/src/main/java/org/nervos/mercury/RpcMethods.java
@@ -10,7 +10,7 @@ public interface RpcMethods {
   String BUILD_DAO_CLAIM_TRANSACTION = "build_dao_claim_transaction";
   String GET_TRANSACTION_INFO = "get_transaction_info";
   String GET_BLOCK_INFO = "get_block_info";
-  String REGISTER_ADDRESS = "register_address";
+  String REGISTER_ADDRESS = "register_addresses";
   String QUERY_TRANSACTIONS = "query_transactions";
   String GET_SPENT_TRANSACTION = "get_spent_transaction";
   String GET_DB_INFO = "get_db_info";
