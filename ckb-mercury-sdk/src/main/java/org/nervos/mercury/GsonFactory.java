43f8846c5f9cdc7241bf3d37d40f9925f81c7ec6
fjchen7 <fjchen7@gmail.com>
2021-11-17 17:12:19 +0800

    feat(mercury): rename and change `AddressOrLockHash` structure for RPC call

--- ckb-mercury-sdk/src/main/java/org/nervos/mercury/GsonFactory.java
+++ ckb-mercury-sdk/src/main/java/org/nervos/mercury/GsonFactory.java
@@ -5,14 +5,14 @@ import com.google.gson.GsonBuilder;
 import org.nervos.mercury.model.common.ExtraFilter;
 import org.nervos.mercury.model.common.RecordStatus;
 import org.nervos.mercury.model.req.item.Item;
-import org.nervos.mercury.model.resp.AddressOrLockHash;
+import org.nervos.mercury.model.resp.Ownership;
 import org.nervos.mercury.model.resp.RecordResponse;
 
 public class GsonFactory {
   public static Gson newGson() {
     Gson g =
         new GsonBuilder()
-            .registerTypeAdapter(AddressOrLockHash.class, new AddressOrLockHash())
+            .registerTypeAdapter(Ownership.class, new Ownership.Deserializer())
             .registerTypeAdapter(RecordStatus.class, new RecordStatus())
             .registerTypeAdapter(RecordResponse.class, new RecordResponse())
             .registerTypeAdapter(ExtraFilter.class, new ExtraFilter())
---
+++ ckb-mercury-sdk/src/main/java/org/nervos/mercury/model/resp/Ownership.java
@@ -0,0 +1,33 @@
+package org.nervos.mercury.model.resp;
+
+import com.google.gson.*;
+import com.google.gson.annotations.SerializedName;
+
+public class Ownership {
+
+  public Type type;
+  public String value;
+
+  public Ownership(Type type, String value) {
+    this.type = type;
+    this.value = value;
+  }
+
+  enum Type {
+    @SerializedName("Address")
+    ADDRESS,
+    @SerializedName("LockHash")
+    LOCKHASH
+  }
+
+  public static class Deserializer implements JsonDeserializer<Ownership> {
+    @Override
+    public Ownership deserialize(
+        JsonElement json, java.lang.reflect.Type typeOfT, JsonDeserializationContext context)
+        throws JsonParseException {
+      Type type = Type.valueOf(json.getAsJsonObject().get("type").getAsString().toUpperCase());
+      String value = json.getAsJsonObject().get("value").getAsString();
+      return new Ownership(type, value);
+    }
+  }
+}
