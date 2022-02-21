dd0bbbe6fbc2b365942eaa8d87dcfd709dfcea6f
fjchen7 <fjchen7@gmail.com>
2022-02-21 19:24:35 +0800

    feat: change ScriptSignerManager to be a class

--- core/src/main/java/org/nervos/ckb/signature/ScriptSignerManager.java
+++ core/src/main/java/org/nervos/ckb/signature/ScriptSignerManager.java
@@ -1,9 +1,37 @@
 package org.nervos.ckb.signature;
 
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 import org.nervos.ckb.type.Script;
+import org.nervos.ckb.type.transaction.Transaction;
 
-public interface ScriptSignerManager {
-  void register(Script script, ScriptSigner scriptSigner);
+public class ScriptSignerManager {
+  private Map<Script, ScriptSigner> scriptSignerMap;
 
-  void signTx(TransactionTemplate transactionTemplate);
+  public ScriptSignerManager() {
+    this.scriptSignerMap = new HashMap<>();
+  }
+
+  // TODO: implement Script' equals() and hashCode()
+  // TODO: clone script to void object being modified
+  // Do we need to register with `ScriptType` as parameter?
+  public ScriptSignerManager register(Script script, ScriptSigner scriptSigner) {
+    scriptSignerMap.put(script, scriptSigner);
+    return this;
+  }
+
+  public void signTx(TransactionTemplate transactionTemplate) {
+    Transaction tx = transactionTemplate.getTxView();
+    List<ScriptGroup> scriptGroups = transactionTemplate.getScriptGroups();
+    for (ScriptGroup group : scriptGroups) {
+      Script script = group.getScript();
+      ScriptSigner signer = scriptSignerMap.get(script);
+      if (signer != null) {
+        signer.signTx(tx, group);
+      } else {
+        throw new RuntimeException("Cannot find ScriptSigner for script " + script);
+      }
+    }
+  }
 }
