e470f81156285435deec2f38078e579b6d2abf3a
fjchen7 <fjchen7@gmail.com>
2022-02-27 15:32:02 +0800

    feat: provide builder for ScriptGroup

--- core/src/main/java/org/nervos/ckb/signature/ScriptGroup.java
+++ core/src/main/java/org/nervos/ckb/signature/ScriptGroup.java
@@ -1,5 +1,6 @@
 package org.nervos.ckb.signature;
 
+import java.util.ArrayList;
 import java.util.List;
 import org.nervos.ckb.type.Script;
 
@@ -39,5 +40,74 @@ public class ScriptGroup {
 
   public void setOutputIndices(List<Integer> outputIndices) {
     this.outputIndices = outputIndices;
+  }
+
+  public static Builder builder() {
+    return new Builder();
+  }
+
+  public static final class Builder {
+    private Script script;
+    private ScriptType scriptType;
+    private List<Integer> inputIndices;
+    private List<Integer> outputIndices;
+
+    private Builder() {
+      this.inputIndices = new ArrayList<>();
+      this.outputIndices = new ArrayList<>();
+    }
+
+    public static Builder aScriptGroup() {
+      return new Builder();
+    }
+
+    public Builder setScript(Script script) {
+      this.script = script;
+      return this;
+    }
+
+    public Builder setScript(String codeHash, String args, String type) {
+      Script script = new Script(codeHash, args, type);
+      return setScript(script);
+    }
+
+    public Builder setScript(String codeHash, String args) {
+      Script script = new Script(codeHash, args, "type");
+      return setScript(script);
+    }
+
+    public Builder setScriptType(ScriptType scriptType) {
+      this.scriptType = scriptType;
+      return this;
+    }
+
+    public Builder setInputIndices(List<Integer> inputIndices) {
+      this.inputIndices = inputIndices;
+      return this;
+    }
+
+    public Builder addInputIndex(int index) {
+      this.inputIndices.add(index);
+      return this;
+    }
+
+    public Builder setOutputIndices(List<Integer> outputIndices) {
+      this.outputIndices = outputIndices;
+      return this;
+    }
+
+    public Builder addOutputIndex(int index) {
+      this.outputIndices.add(index);
+      return this;
+    }
+
+    public ScriptGroup build() {
+      ScriptGroup scriptGroup = new ScriptGroup();
+      scriptGroup.setScript(script);
+      scriptGroup.setScriptType(scriptType);
+      scriptGroup.setInputIndices(inputIndices);
+      scriptGroup.setOutputIndices(outputIndices);
+      return scriptGroup;
+    }
   }
 }
