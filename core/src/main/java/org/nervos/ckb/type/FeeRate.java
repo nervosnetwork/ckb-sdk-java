a0877a4126f0f6a2b5efb2588ee3afdb2091f835
fjchen7 <fjchen7@gmail.com>
2022-03-29 12:41:35 +0800

    Remove FeeRate

--- core/src/main/java/org/nervos/ckb/type/FeeRate.java
+++
@@ -1,9 +0,0 @@
-package org.nervos.ckb.type;
-
-import com.google.gson.annotations.SerializedName;
-
-/** Copyright © 2019 Nervos Foundation. All rights reserved. */
-public class FeeRate {
-  @SerializedName("fee_rate")
-  public String feeRate;
-}
