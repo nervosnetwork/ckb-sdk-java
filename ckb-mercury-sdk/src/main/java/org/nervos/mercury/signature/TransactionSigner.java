d8417eaa2851ab48e7a896ef5d3800518e25781b
fjchen7 <fjchen7@gmail.com>
2022-04-08 16:14:41 +0800

    Comment old transaction sign method for temporary refactor

--- ckb-mercury-sdk/src/main/java/org/nervos/mercury/signature/TransactionSigner.java
+++ ckb-mercury-sdk/src/main/java/org/nervos/mercury/signature/TransactionSigner.java
@@ -20,67 +20,67 @@ public class TransactionSigner {
   }
 
   public void Secp256Blake2bSign(MercuryScriptGroup scriptGroup, String privateKey) {
-    List groupWitnesses = scriptGroup.getGroupWitnesses();
-
-    byte[] txHash = transaction.computeHash();
-    Blake2b blake2b = new Blake2b();
-    blake2b.update(txHash);
-    blake2b.update(
-        new UInt64(Numeric.hexStringToByteArray(scriptGroup.getWitness()).length).toBytes());
-    blake2b.update(Numeric.hexStringToByteArray(scriptGroup.getWitness()));
-
-    for (int i = 1; i < groupWitnesses.size(); i++) {
-      byte[] bytes = Numeric.hexStringToByteArray((String) groupWitnesses.get(i));
-      blake2b.update(new UInt64(bytes.length).toBytes());
-      blake2b.update(bytes);
-    }
-    String message = blake2b.doFinalString();
-    ECKeyPair ecKeyPair = ECKeyPair.createWithPrivateKey(privateKey, false);
-
-    Witness signedWitness = new Witness();
-    signedWitness.lock =
-        Sign.signMessage(Numeric.hexStringToByteArray(message), ecKeyPair).getSignature();
-
-    int replaceIndexStart = scriptGroup.getOffset() * 2 + 2;
-    int replaceIndexEnd = Witness.SIGNATURE_PLACEHOLDER.length * 2 + replaceIndexStart;
-    String newWitness =
-        scriptGroup.getWitness().substring(0, replaceIndexStart)
-            + Numeric.toHexStringNoPrefix(signedWitness.lock)
-            + scriptGroup.getWitness().substring(replaceIndexEnd);
-    transaction.witnesses.set(scriptGroup.getWitnessIndex(), newWitness);
+//    List groupWitnesses = scriptGroup.getGroupWitnesses();
+//
+//    byte[] txHash = transaction.computeHash();
+//    Blake2b blake2b = new Blake2b();
+//    blake2b.update(txHash);
+//    blake2b.update(
+//        new UInt64(Numeric.hexStringToByteArray(scriptGroup.getWitness()).length).toBytes());
+//    blake2b.update(Numeric.hexStringToByteArray(scriptGroup.getWitness()));
+//
+//    for (int i = 1; i < groupWitnesses.size(); i++) {
+//      byte[] bytes = Numeric.hexStringToByteArray((String) groupWitnesses.get(i));
+//      blake2b.update(new UInt64(bytes.length).toBytes());
+//      blake2b.update(bytes);
+//    }
+//    String message = blake2b.doFinalString();
+//    ECKeyPair ecKeyPair = ECKeyPair.createWithPrivateKey(privateKey, false);
+//
+//    Witness signedWitness = new Witness();
+//    signedWitness.lock =
+//        Sign.signMessage(Numeric.hexStringToByteArray(message), ecKeyPair).getSignature();
+//
+//    int replaceIndexStart = scriptGroup.getOffset() * 2 + 2;
+//    int replaceIndexEnd = Witness.SIGNATURE_PLACEHOLDER.length * 2 + replaceIndexStart;
+//    String newWitness =
+//        scriptGroup.getWitness().substring(0, replaceIndexStart)
+//            + Numeric.toHexStringNoPrefix(signedWitness.lock)
+//            + scriptGroup.getWitness().substring(replaceIndexEnd);
+//    transaction.witnesses.set(scriptGroup.getWitnessIndex(), newWitness);
   }
 
   public void KeccakEthereumPersonalSign(MercuryScriptGroup scriptGroup, String privateKey) {
-    List groupWitnesses = scriptGroup.getGroupWitnesses();
-
-    byte[] txHash = transaction.computeHash();
-
-    Keccak256 keccak256 = new Keccak256();
-    keccak256.update(txHash);
-    keccak256.update(
-        new UInt64(Numeric.hexStringToByteArray(scriptGroup.getWitness()).length).toBytes());
-    keccak256.update(Numeric.hexStringToByteArray(scriptGroup.getWitness()));
-
-    for (int i = 1; i < groupWitnesses.size(); i++) {
-      byte[] bytes = Numeric.hexStringToByteArray((String) groupWitnesses.get(i));
-      keccak256.update(new UInt64(bytes.length).toBytes());
-      keccak256.update(bytes);
-    }
-    byte[] messageBytes = keccak256.doFinalBytes();
-
-    ECKeyPair ecKeyPair = ECKeyPair.createWithPrivateKey(privateKey, false);
-    byte[] signature = ethereumPersonalSign(messageBytes, ecKeyPair);
-
-    Witness signedWitness = new Witness();
-    signedWitness.lock = signature;
-
-    int replaceIndexStart = scriptGroup.getOffset() * 2 + 2;
-    int replaceIndexEnd = Witness.SIGNATURE_PLACEHOLDER.length * 2 + replaceIndexStart;
-    String newWitness =
-        scriptGroup.getWitness().substring(0, replaceIndexStart)
-            + Numeric.toHexStringNoPrefix(signedWitness.lock)
-            + scriptGroup.getWitness().substring(replaceIndexEnd);
-    transaction.witnesses.set(scriptGroup.getWitnessIndex(), newWitness);
+//    List groupWitnesses = scriptGroup.getGroupWitnesses();
+//
+//    byte[] txHash = transaction.computeHash();
+//
+//    Keccak256 keccak256 = new Keccak256();
+//    keccak256.update(txHash);
+//    keccak256.update(
+//        new UInt64(Numeric.hexStringToByteArray(scriptGroup.getWitness()).length).toBytes());
+//    keccak256.update(Numeric.hexStringToByteArray(scriptGroup.getWitness()));
+//
+//    for (int i = 1; i < groupWitnesses.size(); i++) {
+//      byte[] bytes = Numeric.hexStringToByteArray((String) groupWitnesses.get(i));
+//      keccak256.update(new UInt64(bytes.length).toBytes());
+//      keccak256.update(bytes);
+//    }
+//    byte[] messageBytes = keccak256.doFinalBytes();
+//
+//    ECKeyPair ecKeyPair = ECKeyPair.createWithPrivateKey(privateKey, false);
+//    byte[] signature = ethereumPersonalSign(messageBytes, ecKeyPair);
+//
+//    Witness signedWitness = new Witness();
+//    signedWitness.lock = signature;
+//
+//    int replaceIndexStart = scriptGroup.getOffset() * 2 + 2;
+//    int replaceIndexEnd = Witness.SIGNATURE_PLACEHOLDER.length * 2 + replaceIndexStart;
+//    String newWitness =
+//        scriptGroup.getWitness().substring(0, replaceIndexStart)
+//            + Numeric.toHexStringNoPrefix(signedWitness.lock)
+//            + scriptGroup.getWitness().substring(replaceIndexEnd);
+//    transaction.witnesses.set(scriptGroup.getWitnessIndex(), newWitness);
   }
 
   private static byte[] ethereumPersonalSign(byte[] message, ECKeyPair ecKeyPair) {
