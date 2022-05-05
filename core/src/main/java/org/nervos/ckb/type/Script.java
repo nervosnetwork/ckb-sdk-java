e2c146e0426d6dd9a5680fac2f79619b4200200b
fjchen7 <fjchen7@gmail.com>
2022-05-05 15:15:45 +0800

    Add static method to generate script from ECKeyPair

--- core/src/main/java/org/nervos/ckb/type/Script.java
+++ core/src/main/java/org/nervos/ckb/type/Script.java
@@ -2,6 +2,8 @@ package org.nervos.ckb.type;
 
 import com.google.gson.annotations.SerializedName;
 import org.nervos.ckb.crypto.Blake2b;
+import org.nervos.ckb.crypto.secp256k1.ECKeyPair;
+import org.nervos.ckb.utils.Numeric;
 import org.nervos.ckb.utils.Utils;
 
 import java.util.Arrays;
@@ -10,6 +12,15 @@ import static org.nervos.ckb.utils.MoleculeConverter.packByte32;
 import static org.nervos.ckb.utils.MoleculeConverter.packBytes;
 
 public class Script {
+  public static final byte[] SECP256_BLAKE160_SIGNHASH_ALL_CODE_HASH =
+      Numeric.hexStringToByteArray("0x9bd7e06f3ecf4be0f2fcd2188b23f1b9fcc88e5d4b65a8637b17723bbda3cce8");
+  public static final byte[] SECP256_BLAKE160_MULTISIG_ALL_CODE_HASH =
+      Numeric.hexStringToByteArray("0x5c5069eb0857efc65e1bca0c07df34c31663b3622fd3876c876320fc9634e2a8");
+  public static final byte[] ANY_CAN_PAY_CODE_HASH_MAINNET =
+      Numeric.hexStringToByteArray("0xd369597ff47f29fbc0d47d2e3775370d1250b85140c670e4718af712983a2354");
+  public static final byte[] ANY_CAN_PAY_CODE_HASH_TESTNET =
+      Numeric.hexStringToByteArray("0x3419a1c09eb2567f6552ee7a8ecffd64155cffe0f1796e6e61ec088d740c1356");
+
   public byte[] codeHash;
   public byte[] args;
   public HashType hashType;
@@ -50,6 +61,20 @@ public class Script {
         .setArgs(packBytes(args))
         .setHashType(hashType.pack())
         .build();
+  }
+
+  public static Script generateSecp256K1Blake160SignhashAllScript(ECKeyPair publicKey) {
+    // CKB uses encoded public keys of compressed form (with prefix 0x04)
+    // See https://en.bitcoin.it/wiki/Elliptic_Curve_Digital_Signature_Algorithm for details
+    byte[] publicKeyBytes = publicKey.getEncodedPublicKey(true);
+    byte[] hash = Blake2b.digest(publicKeyBytes);
+    hash = Arrays.copyOfRange(hash, 0, 20);
+    Script scrip = new Script(
+        SECP256_BLAKE160_SIGNHASH_ALL_CODE_HASH,
+        hash,
+        HashType.TYPE
+    );
+    return scrip;
   }
 
   @Override
