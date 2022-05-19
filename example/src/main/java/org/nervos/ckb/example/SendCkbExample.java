7977dc5001838be3a6eb280acf1930494791f89c
fjchen7 <fjchen7@gmail.com>
2022-05-19 15:31:20 +0800

    Add SendCkbExample

---
+++ example/src/main/java/org/nervos/ckb/example/SendCkbExample.java
@@ -0,0 +1,35 @@
+package org.nervos.ckb.example;
+
+import org.nervos.ckb.Network;
+import org.nervos.ckb.SmartTransactionBuilder;
+import org.nervos.ckb.service.Api;
+import org.nervos.ckb.sign.TransactionSigner;
+import org.nervos.ckb.sign.TransactionWithScriptGroups;
+import org.nervos.ckb.transaction.TransactionInput;
+import org.nervos.ckb.utils.address.Address;
+import org.nervos.indexer.InputIterator;
+
+import java.io.IOException;
+import java.util.Iterator;
+
+public class SendCkbExample {
+  public static void main(String[] args) throws IOException {
+    String sender = "ckt1qzda0cr08m85hc8jlnfp3zer7xulejywt49kt2rr0vthywaa50xwsq2qf8keemy2p5uu0g0gn8cd4ju23s5269qk8rg4r";
+    Network network = Address.decode(sender).getNetwork();
+
+    Iterator<TransactionInput> iterator = new InputIterator(sender);
+    TransactionWithScriptGroups txWithGroups = new SmartTransactionBuilder(network, iterator)
+        .addOutput("ckt1qzda0cr08m85hc8jlnfp3zer7xulejywt49kt2rr0vthywaa50xwsq2qf8keemy2p5uu0g0gn8cd4ju23s5269qk8rg4r",
+                   501)
+        .setFeeRate(1000)
+        .setChangeOutpoint(sender)
+        .build();
+
+    TransactionSigner.getInstance(network)
+        .signTransaction(txWithGroups, "0x6c9ed03816e3111e49384b8d180174ad08e29feb1393ea1b51cef1c505d4e36a");
+
+    Api api = new Api("https://testnet.ckb.dev", false);
+    byte[] txHash = api.sendTransaction(txWithGroups.getTxView());
+    System.out.println("Transaction hash: " + txHash);
+  }
+}
