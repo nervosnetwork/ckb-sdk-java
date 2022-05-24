8413281825b98d399428b037e0feeeb65a43b3b5
fjchen7 <fjchen7@gmail.com>
2022-05-24 22:02:27 +0800

    Asset GetSpentTransaction test cases

--- ckb-api/src/test/java/org/nervos/api/mercury/GetSpentTransactionTest.java
+++ ckb-api/src/test/java/org/nervos/api/mercury/GetSpentTransactionTest.java
@@ -1,10 +1,14 @@
 package org.nervos.api.mercury;
 
 import constant.ApiFactory;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.nervos.ckb.type.OutPoint;
 import org.nervos.ckb.utils.Numeric;
 import org.nervos.mercury.model.GetSpentTransactionPayloadBuilder;
+import org.nervos.mercury.model.resp.TransactionInfoResponse;
+import org.nervos.mercury.model.resp.TransactionWithRichStatus;
+import org.nervos.mercury.model.resp.TxView;
 
 import java.io.IOException;
 
@@ -18,8 +22,10 @@ public class GetSpentTransactionTest {
             Numeric.hexStringToByteArray(
                 "0xb2e952a30656b68044e1d5eed69f1967347248967785449260e3942443cbeece"),
             1));
-
-    ApiFactory.getApi().getSpentTransactionWithTransactionView(builder.build());
+    TxView<TransactionWithRichStatus> txView =
+        ApiFactory.getApi().getSpentTransactionWithTransactionView(builder.build());
+    Assertions.assertNotNull(txView.type);
+    Assertions.assertNotNull(txView.value);
   }
 
   @Test
@@ -30,7 +36,9 @@ public class GetSpentTransactionTest {
             Numeric.hexStringToByteArray(
                 "0xb2e952a30656b68044e1d5eed69f1967347248967785449260e3942443cbeece"),
             1));
-
-    ApiFactory.getApi().getSpentTransactionWithTransactionInfo(builder.build());
+    TxView<TransactionInfoResponse> txView =
+        ApiFactory.getApi().getSpentTransactionWithTransactionInfo(builder.build());
+    Assertions.assertNotNull(txView.type);
+    Assertions.assertNotNull(txView.value);
   }
 }
