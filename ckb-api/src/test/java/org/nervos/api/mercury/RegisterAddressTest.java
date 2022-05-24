2b4b8412ffbd4605b0fdfddd7d22c55873508be2
fjchen7 <fjchen7@gmail.com>
2022-05-24 21:51:53 +0800

    Refactor RegisterAddress RPC

--- ckb-api/src/test/java/org/nervos/api/mercury/RegisterAddressTest.java
+++ ckb-api/src/test/java/org/nervos/api/mercury/RegisterAddressTest.java
@@ -2,6 +2,7 @@ package org.nervos.api.mercury;
 
 import constant.AddressWithKeyHolder;
 import constant.ApiFactory;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.nervos.ckb.utils.address.AddressTools;
 
@@ -12,16 +13,12 @@ import java.util.List;
 public class RegisterAddressTest {
 
   @Test
-  void testRegisterAddress() {
-    try {
-      List<byte[]> scriptHashes =
-          ApiFactory.getApi()
-              .registerAddresses(
-                  Arrays.asList(
-                      AddressTools.generateAcpAddress(AddressWithKeyHolder.testAddress0()).encode()));
-      System.out.println(scriptHashes);
-    } catch (IOException e) {
-      e.printStackTrace();
-    }
+  void testRegisterAddress() throws IOException {
+    List<byte[]> scriptHashes =
+        ApiFactory.getApi().registerAddresses(
+            Arrays.asList(
+                AddressTools.generateAcpAddress(AddressWithKeyHolder.testAddress0()).encode()));
+
+    Assertions.assertTrue(scriptHashes.size() >= 1);
   }
 }
