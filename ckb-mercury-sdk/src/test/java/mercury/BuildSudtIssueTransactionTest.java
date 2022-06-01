4217598daf94bd5c33b617ae5102b12ae8cbf28b
fjchen7 <fjchen7@gmail.com>
2022-06-01 15:26:05 +0800

    Update rpc type SudtIssuePayloadBuilder

--- ckb-mercury-sdk/src/test/java/mercury/BuildSudtIssueTransactionTest.java
+++ ckb-mercury-sdk/src/test/java/mercury/BuildSudtIssueTransactionTest.java
@@ -4,7 +4,10 @@ import constant.ApiFactory;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.nervos.ckb.sign.TransactionWithScriptGroups;
+import org.nervos.ckb.utils.address.Address;
 import org.nervos.mercury.model.SudtIssuePayloadBuilder;
+import org.nervos.mercury.model.req.item.Item;
+import org.nervos.mercury.model.req.item.ItemFactory;
 import org.nervos.mercury.model.req.payload.CapacityProvider;
 
 import java.io.IOException;
@@ -15,9 +18,12 @@ public class BuildSudtIssueTransactionTest {
   @Test
   void testBuildSudtIssueTransaction() throws IOException {
     SudtIssuePayloadBuilder builder = new SudtIssuePayloadBuilder();
-    builder.owner("ckt1qzda0cr08m85hc8jlnfp3zer7xulejywt49kt2rr0vthywaa50xwsqg958atl2zdh8jn3ch8lc72nt0cf864ecqdxm9zf");
+    String secp256SigHashAddress = "ckt1qzda0cr08m85hc8jlnfp3zer7xulejywt49kt2rr0vthywaa50xwsqg958atl2zdh8jn3ch8lc72nt0cf864ecqdxm9zf";
+    builder.owner(secp256SigHashAddress);
     builder.addTo("ckt1qzda0cr08m85hc8jlnfp3zer7xulejywt49kt2rr0vthywaa50xwsqg6flmrtx8y8tuu6s3jf2ahv4l6sjw9hsc3t4tqv", BigInteger.valueOf(1));
     builder.outputCapacityProvider(CapacityProvider.FROM);
+    Item item = ItemFactory.newIdentityItemByCkb(Address.decode(secp256SigHashAddress).getScript().args);
+    builder.addFrom(item);
 
     TransactionWithScriptGroups s =
         ApiFactory.getApi().buildSudtIssueTransaction(builder.build());
