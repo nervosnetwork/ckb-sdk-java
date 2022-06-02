dd8f3c45d3422ca1c276932e7174528af1491f37
fjchen7 <fjchen7@gmail.com>
2022-06-02 18:19:56 +0800

    Rewrite CkbTransactionBuilder

--- core/src/main/java/org/nervos/ckb/transaction/CkbTransactionBuilder.java
+++ core/src/main/java/org/nervos/ckb/transaction/CkbTransactionBuilder.java
@@ -3,33 +3,20 @@ package org.nervos.ckb.transaction;
 import org.nervos.ckb.sign.ScriptGroup;
 import org.nervos.ckb.sign.TransactionWithScriptGroups;
 import org.nervos.ckb.transaction.scriptHandler.ScriptHandler;
-import org.nervos.ckb.type.*;
-import org.nervos.ckb.utils.Calculator;
+import org.nervos.ckb.type.CellOutput;
+import org.nervos.ckb.type.Script;
+import org.nervos.ckb.type.ScriptType;
 import org.nervos.ckb.utils.Utils;
 import org.nervos.ckb.utils.address.Address;
 
 import java.util.*;
 
-public class CkbTransactionBuilder {
-  private int changeOutputIndex = -1;
-  private long feeRate = 1000;
-
-  private List<ScriptHandler> scriptHandlers = new ArrayList<>();
-  private Transaction tx;
-  private Iterator<TransactionInput> availableInputs;
-
+public class CkbTransactionBuilder extends AbstractTransactionBuilder {
   public CkbTransactionBuilder(Iterator<TransactionInput> availableInputs) {
-    tx = new Transaction();
-    tx.version = 0;
-    tx.inputs = new ArrayList<>();
-    tx.outputs = new ArrayList<>();
-    tx.outputsData = new ArrayList<>();
-    tx.cellDeps = new ArrayList<>();
-    tx.headerDeps = new ArrayList<>();
-    tx.witnesses = new ArrayList<>();
-    this.availableInputs = availableInputs;
+    super(availableInputs);
   }
 
+  @Override
   public CkbTransactionBuilder registerScriptHandler(ScriptHandler scriptHandler) {
     scriptHandlers.add(scriptHandler);
     return this;
@@ -72,55 +59,42 @@ public class CkbTransactionBuilder {
   }
 
   public TransactionWithScriptGroups build() {
-    Set<CellDep> cellDeps = new HashSet<>();
     long outputsCapacity = 0L;
-    for (CellOutput output: tx.outputs) {
+    for (CellOutput output : tx.outputs) {
       outputsCapacity += output.capacity;
-      Script script = output.type;
-      for (ScriptHandler handler : scriptHandlers) {
-        if (handler.isMatched(script)) {
-          cellDeps.addAll(handler.getCellDeps());
-        }
-      }
     }
 
     tx.witnesses = new ArrayList<>();
     tx.inputs = new ArrayList<>();
     boolean enoughCapacity = false;
-    Map<Script, ScriptGroup> scriptGroupMap = new HashMap<>();
     long inputsCapacity = 0L;
+    inputsDetail = new ArrayList<>();
+    Map<Script, ScriptGroup> scriptGroupMap = new HashMap<>();
     int inputIndex = -1;
     while (availableInputs.hasNext()) {
       TransactionInput input = availableInputs.next();
-      byte[] witness = new byte[0];
-      inputIndex ++;
+      inputsDetail.add(input);
+      tx.inputs.add(input.input);
+      tx.witnesses.add(new byte[0]);
+      inputIndex += 1;
 
       Script lock = input.output.lock;
       ScriptGroup scriptGroup = scriptGroupMap.get(lock);
-      // only set witness for first appearance of lock in inputs
       if (scriptGroup == null) {
         scriptGroup = new ScriptGroup();
         scriptGroup.setScript(lock);
         scriptGroup.setGroupType(ScriptType.LOCK);
         scriptGroup.getInputIndices().add(inputIndex);
         scriptGroupMap.put(lock, scriptGroup);
-        // add cellDeps and init witness placeholder
-        for (ScriptHandler handler : scriptHandlers) {
-          if (handler.isMatched(lock)) {
-            cellDeps.addAll(handler.getCellDeps());
-            witness = handler.getWitnessPlaceholder(witness);
-          }
-        }
       } else {
         scriptGroup.getInputIndices().add(inputIndex);
       }
+      // add cellDeps and set witness placeholder
+      for (ScriptHandler handler : scriptHandlers) {
+        handler.buildTransaction(this, scriptGroup, null);
+      }
 
-      // set transaction
-      tx.inputs.add(input.input);
-      tx.witnesses.add(witness);
-      tx.cellDeps = new ArrayList<>(cellDeps);
       inputsCapacity += input.output.capacity;
-
       // check if there is enough capacity for output capacity and change
       long fee = calculateTxFee(tx, feeRate);
       long changeCapacity = inputsCapacity - outputsCapacity - fee;
@@ -140,9 +114,5 @@ public class CkbTransactionBuilder {
         .setTxView(tx)
         .setScriptGroups(new ArrayList<>(scriptGroupMap.values()))
         .build();
-  }
-
-  private long calculateTxFee(Transaction transaction, long feeRate) {
-    return Calculator.calculateTransactionFee(transaction, feeRate);
   }
 }
