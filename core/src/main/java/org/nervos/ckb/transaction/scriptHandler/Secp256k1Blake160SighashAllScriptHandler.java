3cf22268ffe49d99561e2ec8f9b11271ea2ed811
fjchen7 <fjchen7@gmail.com>
2022-06-02 15:40:03 +0800

    Implement Secp256k1Blake160SighashAllScriptHandler

---
+++ core/src/main/java/org/nervos/ckb/transaction/scriptHandler/Secp256k1Blake160SighashAllScriptHandler.java
@@ -0,0 +1,58 @@
+package org.nervos.ckb.transaction.scriptHandler;
+
+import org.nervos.ckb.Network;
+import org.nervos.ckb.type.CellDep;
+import org.nervos.ckb.type.OutPoint;
+import org.nervos.ckb.type.Script;
+import org.nervos.ckb.type.WitnessArgs;
+import org.nervos.ckb.utils.Numeric;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class Secp256k1Blake160SighashAllScriptHandler implements ScriptHandler {
+  private List<CellDep> cellDeps;
+
+  public Secp256k1Blake160SighashAllScriptHandler(Network network) {
+    OutPoint outPoint = new OutPoint();
+    if (network == Network.MAINNET) {
+      outPoint.txHash = Numeric.hexStringToByteArray("0x71a7ba8fc96349fea0ed3a5c47992e3b4084b031a42264a018e0072e8172e46c");
+      outPoint.index = 0;
+    } else if (network == Network.TESTNET) {
+      outPoint.txHash = Numeric.hexStringToByteArray("0xf8de3bb47d055cdf460d93a2a6e1b05f7432f9777c8c474abf4eec1d4aee5d37");
+      outPoint.index = 0;
+    } else {
+      throw new IllegalArgumentException("Unsupported network");
+    }
+    CellDep cellDep = new CellDep();
+    cellDep.outPoint = outPoint;
+    cellDep.depType = CellDep.DepType.DEP_GROUP;
+    cellDeps = Arrays.asList(cellDep);
+  }
+
+  @Override
+  public boolean isMatched(Script script) {
+    if (script == null) {
+      return false;
+    }
+    byte[] codeHash = Script.SECP256_BLAKE160_SIGNHASH_ALL_CODE_HASH;
+    return Arrays.equals(script.codeHash, codeHash);
+  }
+
+  @Override
+  public List<CellDep> getCellDeps() {
+    return cellDeps;
+  }
+
+  @Override
+  public byte[] getWitnessPlaceholder(byte[] originalWitness) {
+    WitnessArgs witnessArgs;
+    if (originalWitness == null || originalWitness.length == 0) {
+      witnessArgs = new WitnessArgs();
+    } else {
+      witnessArgs = WitnessArgs.unpack(originalWitness);
+    }
+    witnessArgs.setLock(new byte[65]);
+    return witnessArgs.pack().toByteArray();
+  }
+}
