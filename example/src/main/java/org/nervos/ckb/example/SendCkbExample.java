55e247f66fba24179d23013562e60ea5f801ca1b
fjchen7 <fjchen7@gmail.com>
2022-06-02 15:40:24 +0800

    Update SendCkbExample

--- example/src/main/java/org/nervos/ckb/example/SendCkbExample.java
+++ example/src/main/java/org/nervos/ckb/example/SendCkbExample.java
@@ -1,12 +1,13 @@
 package org.nervos.ckb.example;
 
 import org.nervos.ckb.Network;
-import org.nervos.ckb.SmartTransactionBuilder;
 import org.nervos.ckb.service.Api;
+import org.nervos.ckb.service.GsonFactory;
 import org.nervos.ckb.sign.TransactionSigner;
 import org.nervos.ckb.sign.TransactionWithScriptGroups;
+import org.nervos.ckb.transaction.CkbTransactionBuilder;
 import org.nervos.ckb.transaction.TransactionInput;
-import org.nervos.ckb.utils.address.Address;
+import org.nervos.ckb.transaction.scriptHandler.Secp256k1Blake160SighashAllScriptHandler;
 import org.nervos.indexer.InputIterator;
 
 import java.io.IOException;
@@ -15,17 +16,18 @@ import java.util.Iterator;
 public class SendCkbExample {
   public static void main(String[] args) throws IOException {
     String sender = "ckt1qzda0cr08m85hc8jlnfp3zer7xulejywt49kt2rr0vthywaa50xwsq2qf8keemy2p5uu0g0gn8cd4ju23s5269qk8rg4r";
-    Network network = Address.decode(sender).getNetwork();
 
     Iterator<TransactionInput> iterator = new InputIterator(sender);
-    TransactionWithScriptGroups txWithGroups = new SmartTransactionBuilder(network, iterator)
+    TransactionWithScriptGroups txWithGroups = new CkbTransactionBuilder(iterator)
+        .registerScriptHandler(new Secp256k1Blake160SighashAllScriptHandler(Network.TESTNET))
         .addOutput("ckt1qzda0cr08m85hc8jlnfp3zer7xulejywt49kt2rr0vthywaa50xwsq2qf8keemy2p5uu0g0gn8cd4ju23s5269qk8rg4r",
                    501)
         .setFeeRate(1000)
         .setChangeOutpoint(sender)
         .build();
+    System.out.println(GsonFactory.create().toJson(txWithGroups));
 
-    TransactionSigner.getInstance(network)
+    TransactionSigner.getInstance(Network.TESTNET)
         .signTransaction(txWithGroups, "0x6c9ed03816e3111e49384b8d180174ad08e29feb1393ea1b51cef1c505d4e36a");
 
     Api api = new Api("https://testnet.ckb.dev", false);
