ec66e5f940faeff61e0c953e16b890a1ccee2763
fjchen7 <fjchen7@gmail.com>
2022-06-07 17:03:12 +0800

    Add method for sudt amount conversion

--- core/src/main/java/org/nervos/ckb/utils/AmountUtils.java
+++ core/src/main/java/org/nervos/ckb/utils/AmountUtils.java
@@ -1,5 +1,7 @@
 package org.nervos.ckb.utils;
 
+import java.math.BigInteger;
+
 public class AmountUtils {
   public static long ckbToShannon(long value) {
     return Utils.ckbToShannon(value);
@@ -7,5 +9,18 @@ public class AmountUtils {
 
   public static long ckbToShannon(double value) {
     return Utils.ckbToShannon(value);
+  }
+
+
+  public static byte[] SudtAmountToData(BigInteger sudtAmount) {
+    return MoleculeConverter.packUint128(sudtAmount).toByteArray();
+  }
+
+  public static BigInteger dataToSudtAmount(byte[] data) {
+    byte[] reversedData = new byte[data.length];
+    for (int i = 0; i < data.length; i++) {
+      reversedData[i] = data[data.length - i - 1];
+    }
+    return new BigInteger(1, reversedData);
   }
 }
