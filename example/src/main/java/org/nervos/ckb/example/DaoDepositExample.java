72c235a4f1c0f6dafff9a676df3b10e2359a9e17
fjchen7 <fjchen7@gmail.com>
2022-06-10 16:49:53 +0800

    Update DaoDepositExample

--- example/src/main/java/org/nervos/ckb/example/DaoDepositExample.java
+++ example/src/main/java/org/nervos/ckb/example/DaoDepositExample.java
@@ -5,34 +5,22 @@ import org.nervos.ckb.service.Api;
 import org.nervos.ckb.sign.TransactionSigner;
 import org.nervos.ckb.sign.TransactionWithScriptGroups;
 import org.nervos.ckb.transaction.CkbTransactionBuilder;
-import org.nervos.ckb.type.CellOutput;
-import org.nervos.ckb.type.Script;
 import org.nervos.ckb.type.TransactionInput;
 import org.nervos.ckb.utils.Numeric;
-import org.nervos.ckb.utils.address.Address;
 import org.nervos.indexer.InputIterator;
 
 import java.io.IOException;
 import java.util.Iterator;
 
 public class DaoDepositExample {
-  private static byte[] depositDaoData = Numeric.hexStringToByteArray("0x0000000000000000");
-  private static Script daoScript = new Script(Script.DAO_CODE_HASH,
-                                               new byte[0],
-                                               Script.HashType.TYPE);
-
   public static void main(String[] args) throws IOException {
     Network network = Network.TESTNET;
     String sender = "ckt1qzda0cr08m85hc8jlnfp3zer7xulejywt49kt2rr0vthywaa50xwsq2qf8keemy2p5uu0g0gn8cd4ju23s5269qk8rg4r";
-    // Deposit 500 CKB to DAO
-    CellOutput output = new CellOutput(50000000000L,
-                                       Address.decode(sender).getScript(),
-                                       daoScript);
 
-    // Construct transaction
+    // Construct transaction to deposit 510 CKB to DAO
     Iterator<TransactionInput> iterator = new InputIterator(sender);
     TransactionWithScriptGroups txWithGroups = new CkbTransactionBuilder(iterator, network)
-        .addOutput(output, depositDaoData)
+        .addDaoDepositOutput(sender, 51000000000L)
         .setFeeRate(1000)
         .setChangeOutput(sender)
         .build();
