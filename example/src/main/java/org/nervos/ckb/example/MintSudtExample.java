5c1c7816e438aeb0796a9c626fa645c8460fd1b6
fjchen7 <fjchen7@gmail.com>
2022-06-10 18:17:39 +0800

    Add MintSudtExample

---
+++ example/src/main/java/org/nervos/ckb/example/MintSudtExample.java
@@ -0,0 +1,39 @@
+package org.nervos.ckb.example;
+
+import org.nervos.ckb.Network;
+import org.nervos.ckb.service.Api;
+import org.nervos.ckb.sign.TransactionSigner;
+import org.nervos.ckb.sign.TransactionWithScriptGroups;
+import org.nervos.ckb.transaction.SudtTransactionBuilder;
+import org.nervos.ckb.type.TransactionInput;
+import org.nervos.ckb.utils.Numeric;
+import org.nervos.indexer.InputIterator;
+
+import java.io.IOException;
+import java.util.Iterator;
+
+public class MintSudtExample {
+  public static void main(String[] args) throws IOException {
+    Network network = Network.TESTNET;
+    String sender = "ckt1qzda0cr08m85hc8jlnfp3zer7xulejywt49kt2rr0vthywaa50xwsqdamwzrffgc54ef48493nfd2sd0h4cjnxg4850up";
+
+    // Construct transaction to deposit 510 CKB to DAO
+    Iterator<TransactionInput> iterator = new InputIterator(sender);
+    TransactionWithScriptGroups txWithGroups = new SudtTransactionBuilder(iterator, network)
+        .setTransactionType(SudtTransactionBuilder.TransactionType.ISSUE)
+        .setSudtArgs(sender)
+        .addSudtOutput(sender, 51000000000L, 10L)
+        .setFeeRate(1000)
+        .setChangeOutput(sender)
+        .build();
+
+    // Sign transaction
+    TransactionSigner.getInstance(network)
+        .signTransaction(txWithGroups, "0x0c982052ffd4af5f3bbf232301dcddf468009161fc48ba1426e3ce0929fb59f8");
+
+    // Send transaction
+    Api api = new Api("https://testnet.ckb.dev", false);
+    byte[] txHash = api.sendTransaction(txWithGroups.getTxView());
+    System.out.println("Transaction hash: " + Numeric.toHexString(txHash));
+  }
+}
