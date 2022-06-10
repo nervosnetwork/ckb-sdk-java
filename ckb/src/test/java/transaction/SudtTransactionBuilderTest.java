05336e023b6a689238c326d1634b0b65a047004f
fjchen7 <fjchen7@gmail.com>
2022-06-10 18:47:11 +0800

    Update builder test

--- ckb/src/test/java/transaction/SudtTransactionBuilderTest.java
+++ ckb/src/test/java/transaction/SudtTransactionBuilderTest.java
@@ -18,58 +18,33 @@ import static org.nervos.ckb.utils.AmountUtils.dataToSudtAmount;
 import static org.nervos.ckb.utils.AmountUtils.sudtAmountToData;
 
 class SudtTransactionBuilderTest {
+  byte[] sudtArgs = Numeric.hexStringToByteArray("0xae4147ba8412767b3fd9bd16d45dab2fa5df283a6fd68dae5367524daa767ca7");
   byte[] fakeHash = Numeric.hexStringToByteArray("0x0000000000000000000000000000000000000000000000000000000000000000");
   Address sender = Address.decode("ckt1qzda0cr08m85hc8jlnfp3zer7xulejywt49kt2rr0vthywaa50xwsq02cgdvd5mng9924xarf3rflqzafzmzlpsuhh83c");
   Script lock = sender.getScript();
   Script type = new Script(
-        Numeric.hexStringToByteArray("0xc5e5dcf215925f7ef4dfaf5f4b4f105bc321c02776d6e7d52a1db3fcd9d011a4"),
-        Numeric.hexStringToByteArray("0xae4147ba8412767b3fd9bd16d45dab2fa5df283a6fd68dae5367524daa767ca7"),
-        Script.HashType.TYPE);
-
+      Numeric.hexStringToByteArray("0xc5e5dcf215925f7ef4dfaf5f4b4f105bc321c02776d6e7d52a1db3fcd9d011a4"),
+      sudtArgs,
+      Script.HashType.TYPE);
+ 
   @Test
-  void testSingleInput() {
-    CellOutput output = new CellOutput(501, lock, type);
-    CellOutput changeOutput = new CellOutput(0, lock, type);
-
+  void testBalance() {
     Iterator<TransactionInput> iterator = newTransactionInputs();
     TransactionWithScriptGroups txWithGroups = new SudtTransactionBuilder(iterator, Network.TESTNET)
-        .addOutput(output, BigInteger.valueOf(99))
+        .setTransactionType(SudtTransactionBuilder.TransactionType.TRANSFER)
+        .setSudtArgs(sudtArgs)
+        .addSudtOutput("ckt1qzda0cr08m85hc8jlnfp3zer7xulejywt49kt2rr0vthywaa50xwsqdamwzrffgc54ef48493nfd2sd0h4cjnxg4850up",
+                       20000000000L, 1L)
         .setFeeRate(1000)
-        .setChangeOutput(changeOutput)
+        .setChangeOutput("ckt1qzda0cr08m85hc8jlnfp3zer7xulejywt49kt2rr0vthywaa50xwsqdamwzrffgc54ef48493nfd2sd0h4cjnxg4850up")
         .build();
 
     Transaction tx = txWithGroups.getTxView();
-    Assertions.assertEquals(1, tx.inputs.size());
-    Assertions.assertEquals(2, txWithGroups.scriptGroups.size());
-    Assertions.assertEquals(2, tx.outputs.size());
-    long fee = 100000000000L - tx.outputs.get(0).capacity - tx.outputs.get(1).capacity;
-    Assertions.assertEquals(703, fee);
-    BigInteger outputSudtAmount = dataToSudtAmount(tx.outputsData.get(0));
-    BigInteger changeSudtAmount = dataToSudtAmount(tx.outputsData.get(1));
-    Assertions.assertEquals(BigInteger.valueOf(100L), outputSudtAmount.add(changeSudtAmount));
-  }
-
-  @Test
-  void testMultipleInput() {
-    CellOutput output = new CellOutput(501, lock, type);
-    CellOutput changeOutput = new CellOutput(0, lock, type);
-
-    Iterator<TransactionInput> iterator = newTransactionInputs();
-    TransactionWithScriptGroups txWithGroups = new SudtTransactionBuilder(iterator, Network.TESTNET)
-        .addOutput(output, BigInteger.valueOf(105L))
-        .setFeeRate(1000)
-        .setChangeOutput(changeOutput)
-        .build();
-
-    Transaction tx = txWithGroups.getTxView();
-    Assertions.assertEquals(2, tx.inputs.size());
-    Assertions.assertEquals(2, txWithGroups.scriptGroups.size());
-    Assertions.assertEquals(2, tx.outputs.size());
-    long fee = 110000000000L - tx.outputs.get(0).capacity - tx.outputs.get(1).capacity;
-    Assertions.assertEquals(755, fee);
-    BigInteger outputSudtAmount = dataToSudtAmount(tx.outputsData.get(0));
-    BigInteger changeSudtAmount = dataToSudtAmount(tx.outputsData.get(1));
-    Assertions.assertEquals(BigInteger.valueOf(110L), outputSudtAmount.add(changeSudtAmount));
+    BigInteger amount1 = dataToSudtAmount(tx.outputsData.get(0));
+    BigInteger amount2 = dataToSudtAmount(tx.outputsData.get(1));
+    Assertions.assertEquals(
+        BigInteger.valueOf(100L),
+        amount1.add(amount2));
   }
 
   private Iterator<TransactionInput> newTransactionInputs() {
@@ -80,7 +55,7 @@ class SudtTransactionBuilderTest {
     byte[] data = sudtAmountToData(BigInteger.valueOf(100L));  // 100 SUDT
     inputs.add(new TransactionInput(cellInput, cellOutput, data));
 
-    cellInput = new CellInput(new OutPoint(fakeHash, 0), 0);
+    cellInput = new CellInput(new OutPoint(fakeHash, 1), 0);
     cellOutput = new CellOutput(10000000000L, lock, type); // 100 CKB
     data = sudtAmountToData(BigInteger.valueOf(10L));  // 10 SUDT
     inputs.add(new TransactionInput(cellInput, cellOutput, data));
