53a40efe0d6fe8c69436d291c119b3b4762635b5
fjchen7 <fjchen7@gmail.com>
2022-06-13 13:28:06 +0800

    Update mercury endpoint

--- ckb-mercury-sdk/src/test/java/constant/ApiFactory.java
+++ ckb-mercury-sdk/src/test/java/constant/ApiFactory.java
@@ -5,7 +5,7 @@ import org.nervos.mercury.MercuryApi;
 
 public class ApiFactory {
 
-  private static String MERCURY_URL = "https://mercury-testnet.ckbapp.dev/";
+  private static String MERCURY_URL = "https://mercury-testnet.ckbapp.dev/0.4";
 
   private static MercuryApi API = new DefaultMercuryApi(MERCURY_URL, false);
 
