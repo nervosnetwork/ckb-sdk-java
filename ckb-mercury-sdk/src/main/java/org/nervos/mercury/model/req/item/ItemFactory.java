7c0cb4933d95c9ae3473a55c0882df4a1c154266
fjchen7 <fjchen7@gmail.com>
2022-06-14 20:28:18 +0800

    Replace method `newIdentityItemBySecp256k1Blake160SignhashAllAddress` with `newIdentityItemByAddress`

--- ckb-mercury-sdk/src/main/java/org/nervos/mercury/model/req/item/ItemFactory.java
+++ ckb-mercury-sdk/src/main/java/org/nervos/mercury/model/req/item/ItemFactory.java
@@ -1,13 +1,15 @@
 package org.nervos.mercury.model.req.item;
 
 import org.bouncycastle.util.encoders.Hex;
+import org.nervos.ckb.Network;
 import org.nervos.ckb.type.OutPoint;
 import org.nervos.ckb.type.Script;
 import org.nervos.ckb.utils.Numeric;
+import org.nervos.ckb.utils.address.Address;
 
 import java.util.Arrays;
 
-import static org.nervos.ckb.type.Script.SECP256_BLAKE160_SIGNHASH_ALL_CODE_HASH;
+import static org.nervos.ckb.type.Script.*;
 
 // https://github.com/XuJiandong/docs-bank/blob/master/omni_lock.md#auth
 public class ItemFactory {
@@ -30,16 +32,36 @@ public class ItemFactory {
     return newIdentityItemByCkb(Numeric.hexStringToByteArray(pubKeyHash));
   }
 
-  public static Item newIdentityItemBySecp256k1Blake160SignhashAllAddress(String address) {
-    org.nervos.ckb.utils.address.Address addr = org.nervos.ckb.utils.address.Address.decode(address);
-    Script script = addr.getScript();
-    if (!Arrays.equals(SECP256_BLAKE160_SIGNHASH_ALL_CODE_HASH, script.codeHash)) {
-      throw new IllegalArgumentException("address is not a SECP256_BLAKE160_SIGNHASH_ALL address");
-    }
-    if (script.hashType != Script.HashType.TYPE) {
-      throw new IllegalArgumentException("address hash type should be TYPE");
+  /**
+   * Create a new identity item from secp256_blake160_signhash_all or ACP address
+   *
+   * @param address a secp256_blake160_signhash_all or ACP address
+   * @return a new identity item
+   */
+  public static Item newIdentityItemByAddress(String address) {
+    Address a = Address.decode(address);
+    Script script = a.getScript();
+    if (!isValidAddress(a)) {
+      throw new IllegalArgumentException("not a valid secp256_blake160_signhash_all or ACP address");
     }
     return newIdentityItemByCkb(script.args);
+  }
+
+  /**
+   * Check if the address is a valid secp256_blake160_signhash_all or ACP address.
+   */
+  private static boolean isValidAddress(Address address) {
+    Script script = address.getScript();
+    Network network = address.getNetwork();
+    if (script.hashType != Script.HashType.TYPE) {
+      return false;
+    }
+    if (Arrays.equals(SECP256_BLAKE160_SIGNHASH_ALL_CODE_HASH, script.codeHash)
+        || (Network.TESTNET == network && Arrays.equals(ANY_CAN_PAY_CODE_HASH_TESTNET, script.codeHash)
+        || (Network.MAINNET == network && Arrays.equals(ANY_CAN_PAY_CODE_HASH_MAINNET, script.codeHash)))) {
+      return true;
+    }
+    return false;
   }
 
   public static Item newOutPointItem(byte[] txHash, int index) {
