029e089d458221cad5efbad538bd049c5ed36ddd
fjchen7 <fjchen7@gmail.com>
2022-09-16 20:22:55 +0800

    Reindent files

--- core/src/main/java/org/nervos/ckb/sign/omnilock/molecule/Auth.java
+++ core/src/main/java/org/nervos/ckb/sign/omnilock/molecule/Auth.java
@@ -8,87 +8,87 @@ import javax.annotation.Nullable;
 import java.util.Objects;
 
 public final class Auth extends Array {
-    public static Class ITEM_TYPE = byte.class;
+  public static Class ITEM_TYPE = byte.class;
 
-    public static int ITEM_SIZE = 1;
+  public static int ITEM_SIZE = 1;
 
-    public static int ITEM_COUNT = 21;
+  public static int ITEM_COUNT = 21;
 
-    public static int SIZE = ITEM_SIZE * ITEM_COUNT;
+  public static int SIZE = ITEM_SIZE * ITEM_COUNT;
 
+  private byte[] items;
+
+  private Auth() {
+  }
+
+  @Nonnull
+  public byte get(int i) {
+    return items[i];
+  }
+
+  @Nullable
+  public byte[] getItems() {
+    return items;
+  }
+
+  @Override
+  public int getItemCount() {
+    return ITEM_COUNT;
+  }
+
+  @Override
+  public int getItemSize() {
+    return ITEM_SIZE;
+  }
+
+  @Override
+  public Class getItemType() {
+    return ITEM_TYPE;
+  }
+
+  public static Builder builder() {
+    return new Builder();
+  }
+
+  public static Builder builder(@Nonnull byte[] buf) {
+    return new Builder(buf);
+  }
+
+  public static final class Builder {
     private byte[] items;
 
-    private Auth() {
+    private Builder() {
+      items = new byte[ITEM_COUNT];
     }
 
-    @Nonnull
-    public byte get(int i) {
-        return items[i];
+    private Builder(@Nonnull byte[] buf) {
+      Objects.requireNonNull(buf);
+      if (buf.length != SIZE) {
+        throw MoleculeException.invalidByteSize(SIZE, buf.length, Auth.class);
+      }
+      items = buf;
     }
 
-    @Nullable
-    public byte[] getItems() {
-        return items;
+    public Builder set(int i, @Nonnull byte item) {
+      Objects.requireNonNull(item);
+      items[i] = item;
+      return this;
     }
 
-    @Override
-    public int getItemCount() {
-        return ITEM_COUNT;
+    public Builder set(@Nonnull byte[] items) {
+      Objects.requireNonNull(items);
+      if (items.length != ITEM_COUNT) {
+        throw MoleculeException.invalidItemCount(ITEM_COUNT, items.length, Auth.class);
+      }
+      this.items = items;
+      return this;
     }
 
-    @Override
-    public int getItemSize() {
-        return ITEM_SIZE;
+    public Auth build() {
+      Auth a = new Auth();
+      a.buf = items;
+      a.items = items;
+      return a;
     }
-
-    @Override
-    public Class getItemType() {
-        return ITEM_TYPE;
-    }
-
-    public static Builder builder() {
-        return new Builder();
-    }
-
-    public static Builder builder(@Nonnull byte[] buf) {
-        return new Builder(buf);
-    }
-
-    public static final class Builder {
-        private byte[] items;
-
-        private Builder() {
-            items = new byte[ITEM_COUNT];
-        }
-
-        private Builder(@Nonnull byte[] buf) {
-            Objects.requireNonNull(buf);
-            if (buf.length != SIZE) {
-                throw MoleculeException.invalidByteSize(SIZE, buf.length, Auth.class);
-            }
-            items = buf;
-        }
-
-        public Builder set(int i, @Nonnull byte item) {
-            Objects.requireNonNull(item);
-            items[i] = item;
-            return this;
-        }
-
-        public Builder set(@Nonnull byte[] items) {
-            Objects.requireNonNull(items);
-            if (items.length != ITEM_COUNT) {
-                throw MoleculeException.invalidItemCount(ITEM_COUNT, items.length, Auth.class);
-            }
-            this.items = items;
-            return this;
-        }
-
-        public Auth build() {
-            Auth a = new Auth();
-            a.buf = items;
-            a.items = items;
-            return a;
-        }
-    }
+  }
 }
--- core/src/main/java/org/nervos/ckb/sign/omnilock/molecule/Identity.java
+++ core/src/main/java/org/nervos/ckb/sign/omnilock/molecule/Identity.java
@@ -9,101 +9,101 @@ import java.util.Arrays;
 import java.util.Objects;
 
 public final class Identity extends Table {
-    public static int FIELD_COUNT = 2;
+  public static int FIELD_COUNT = 2;
 
+  private Auth identity;
+
+  private SmtProofEntryVec proofs;
+
+  private Identity() {
+  }
+
+  @Nonnull
+  public Auth getIdentity() {
+    return identity;
+  }
+
+  @Nonnull
+  public SmtProofEntryVec getProofs() {
+    return proofs;
+  }
+
+  public static Builder builder() {
+    return new Builder();
+  }
+
+  public static Builder builder(@Nonnull byte[] buf) {
+    return new Builder(buf);
+  }
+
+  public static final class Builder {
     private Auth identity;
 
     private SmtProofEntryVec proofs;
 
-    private Identity() {
+    private Builder() {
+      identity = Auth.builder().build();
+      proofs = SmtProofEntryVec.builder().build();
     }
 
-    @Nonnull
-    public Auth getIdentity() {
-        return identity;
+    private Builder(@Nonnull byte[] buf) {
+      Objects.requireNonNull(buf);
+      int size = MoleculeUtils.littleEndianBytes4ToInt(buf, 0);
+      if (buf.length != size) {
+        throw MoleculeException.invalidByteSize(size, buf.length, Identity.class);
+      }
+      int[] offsets = MoleculeUtils.getOffsets(buf);
+      if (offsets.length - 1 != FIELD_COUNT) {
+        throw MoleculeException.invalidFieldCount(FIELD_COUNT, offsets.length - 1, Identity.class);
+      }
+      byte[] itemBuf;
+      itemBuf = Arrays.copyOfRange(buf, offsets[0], offsets[1]);
+      identity = Auth.builder(itemBuf).build();
+      itemBuf = Arrays.copyOfRange(buf, offsets[1], offsets[2]);
+      proofs = SmtProofEntryVec.builder(itemBuf).build();
     }
 
-    @Nonnull
-    public SmtProofEntryVec getProofs() {
-        return proofs;
+    public Builder setIdentity(@Nonnull Auth identity) {
+      Objects.requireNonNull(identity);
+      this.identity = identity;
+      return this;
     }
 
-    public static Builder builder() {
-        return new Builder();
+    public Builder setProofs(@Nonnull SmtProofEntryVec proofs) {
+      Objects.requireNonNull(proofs);
+      this.proofs = proofs;
+      return this;
     }
 
-    public static Builder builder(@Nonnull byte[] buf) {
-        return new Builder(buf);
+    public Identity build() {
+      int[] offsets = new int[FIELD_COUNT];
+      offsets[0] = 4 + 4 * FIELD_COUNT;
+      offsets[1] = offsets[0] + identity.getSize();
+      int[] fieldsSize = new int[FIELD_COUNT];
+      fieldsSize[0] = identity.getSize();
+      fieldsSize[1] = proofs.getSize();
+      byte[][] fieldsBuf = new byte[FIELD_COUNT][];
+      fieldsBuf[0] = identity.toByteArray();
+      fieldsBuf[1] = proofs.toByteArray();
+      int size = 4 + 4 * FIELD_COUNT;
+      for (int i = 0; i < FIELD_COUNT; i++) {
+        size += fieldsSize[i];
+      }
+      byte[] buf = new byte[size];
+      MoleculeUtils.setInt(size, buf, 0);
+      int start = 4;
+      for (int i = 0; i < FIELD_COUNT; i++) {
+        MoleculeUtils.setInt(offsets[i], buf, start);
+        start += 4;
+      }
+      for (int i = 0; i < FIELD_COUNT; i++) {
+        MoleculeUtils.setBytes(fieldsBuf[i], buf, offsets[i]);
+      }
+      Identity t = new Identity();
+      t.buf = buf;
+      t.identity = identity;
+      t.proofs = proofs;
+      return t;
     }
-
-    public static final class Builder {
-        private Auth identity;
-
-        private SmtProofEntryVec proofs;
-
-        private Builder() {
-            identity = Auth.builder().build();
-            proofs = SmtProofEntryVec.builder().build();
-        }
-
-        private Builder(@Nonnull byte[] buf) {
-            Objects.requireNonNull(buf);
-            int size = MoleculeUtils.littleEndianBytes4ToInt(buf, 0);
-            if (buf.length != size) {
-                throw MoleculeException.invalidByteSize(size, buf.length, Identity.class);
-            }
-            int[] offsets = MoleculeUtils.getOffsets(buf);
-            if (offsets.length - 1 != FIELD_COUNT) {
-                throw MoleculeException.invalidFieldCount(FIELD_COUNT, offsets.length - 1, Identity.class);
-            }
-            byte[] itemBuf;
-            itemBuf = Arrays.copyOfRange(buf, offsets[0], offsets[1]);
-            identity = Auth.builder(itemBuf).build();
-            itemBuf = Arrays.copyOfRange(buf, offsets[1], offsets[2]);
-            proofs = SmtProofEntryVec.builder(itemBuf).build();
-        }
-
-        public Builder setIdentity(@Nonnull Auth identity) {
-            Objects.requireNonNull(identity);
-            this.identity = identity;
-            return this;
-        }
-
-        public Builder setProofs(@Nonnull SmtProofEntryVec proofs) {
-            Objects.requireNonNull(proofs);
-            this.proofs = proofs;
-            return this;
-        }
-
-        public Identity build() {
-            int[] offsets = new int[FIELD_COUNT];
-            offsets[0] = 4 + 4 * FIELD_COUNT;
-            offsets[1] = offsets[0] + identity.getSize();
-            int[] fieldsSize = new int[FIELD_COUNT];
-            fieldsSize[0] = identity.getSize();
-            fieldsSize[1] = proofs.getSize();
-            byte[][] fieldsBuf = new byte[FIELD_COUNT][];
-            fieldsBuf[0] = identity.toByteArray();
-            fieldsBuf[1] = proofs.toByteArray();
-            int size = 4 + 4 * FIELD_COUNT;
-            for (int i = 0; i < FIELD_COUNT; i++) {
-                size += fieldsSize[i];
-            }
-            byte[] buf = new byte[size];
-            MoleculeUtils.setInt(size, buf, 0);
-            int start = 4;
-            for (int i = 0; i < FIELD_COUNT; i++) {
-                MoleculeUtils.setInt(offsets[i], buf, start);
-                start += 4;
-            }
-            for (int i = 0; i < FIELD_COUNT; i++) {
-                MoleculeUtils.setBytes(fieldsBuf[i], buf, offsets[i]);
-            }
-            Identity t = new Identity();
-            t.buf = buf;
-            t.identity = identity;
-            t.proofs = proofs;
-            return t;
-        }
-    }
+  }
 }
