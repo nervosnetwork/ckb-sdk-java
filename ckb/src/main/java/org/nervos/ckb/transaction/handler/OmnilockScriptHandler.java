fd6d555c25bdf288783d82d85e72eceabfa53ba0
fjchen7 <fjchen7@gmail.com>
2022-09-16 19:18:14 +0800

    Config celldep for single and multi sign in OmnilockScriptHandler

--- ckb/src/main/java/org/nervos/ckb/transaction/handler/OmnilockScriptHandler.java
+++ ckb/src/main/java/org/nervos/ckb/transaction/handler/OmnilockScriptHandler.java
@@ -8,55 +8,51 @@ import org.nervos.ckb.sign.omnilock.OmnilockWitnessLock;
 import org.nervos.ckb.sign.signer.Secp256k1Blake160MultisigAllSigner;
 import org.nervos.ckb.transaction.AbstractTransactionBuilder;
 import org.nervos.ckb.type.CellDep;
-import org.nervos.ckb.type.OutPoint;
 import org.nervos.ckb.type.Script;
 import org.nervos.ckb.type.WitnessArgs;
-import org.nervos.ckb.utils.Numeric;
 
 import java.util.Arrays;
 import java.util.Objects;
 
 public class OmnilockScriptHandler implements ScriptHandler {
   private CellDep cellDep;
+  private CellDep singleSignCellDep;
+  private CellDep multiSignCellDep;
   private byte[] codeHash;
 
-  // AUTH Mode:
-  // 0x00
-  // 0x01 ~ 0x05: fixed length witness
-  // 0x06: Multisig context
-  // 0xFC: input - set by user??
-  // 0xFD?
-  // 0xFE
-
-
-  // ADMINISTRATOR Mode:
-  // 0x00: celldeps - adminList cell
-  // 0xFC: celldeps - adminList cell
-  //       input - proof cell
-  // 0xFC: input - adminList cell
-  //       input - proof cell
-
-  public OmnilockScriptHandler(CellDep cellDep, byte[] codeHash) {
-    this.cellDep = cellDep;
-    this.codeHash = codeHash;
+  public OmnilockScriptHandler() {
   }
 
   public OmnilockScriptHandler(Network network) {
-    OutPoint outPoint = new OutPoint();
     if (network == Network.MAINNET) {
-      outPoint.txHash = Numeric.hexStringToByteArray("0xdfdb40f5d229536915f2d5403c66047e162e25dedd70a79ef5164356e1facdc8");
-      outPoint.index = 0;
       codeHash = Script.OMNILOCK_CODE_HASH_MAINNET;
+      cellDep = new CellDep("0xdfdb40f5d229536915f2d5403c66047e162e25dedd70a79ef5164356e1facdc8", 0, CellDep.DepType.CODE);
+      singleSignCellDep = new CellDep("0x71a7ba8fc96349fea0ed3a5c47992e3b4084b031a42264a018e0072e8172e46c", 0, CellDep.DepType.DEP_GROUP);
+      multiSignCellDep = new CellDep("0x71a7ba8fc96349fea0ed3a5c47992e3b4084b031a42264a018e0072e8172e46c", 1, CellDep.DepType.DEP_GROUP);
     } else if (network == Network.TESTNET) {
-      outPoint.txHash = Numeric.hexStringToByteArray("0x27b62d8be8ed80b9f56ee0fe41355becdb6f6a40aeba82d3900434f43b1c8b60");
-      outPoint.index = 0;
       codeHash = Script.OMNILOCK_CODE_HASH_TESTNET;
+      cellDep = new CellDep("0x27b62d8be8ed80b9f56ee0fe41355becdb6f6a40aeba82d3900434f43b1c8b60", 0, CellDep.DepType.CODE);
+      singleSignCellDep = new CellDep("0xf8de3bb47d055cdf460d93a2a6e1b05f7432f9777c8c474abf4eec1d4aee5d37", 0, CellDep.DepType.DEP_GROUP);
+      multiSignCellDep = new CellDep("0xf8de3bb47d055cdf460d93a2a6e1b05f7432f9777c8c474abf4eec1d4aee5d37", 1, CellDep.DepType.DEP_GROUP);
     } else {
       throw new IllegalArgumentException("Unsupported network: " + network);
     }
-    cellDep = new CellDep();
-    cellDep.outPoint = outPoint;
-    cellDep.depType = CellDep.DepType.CODE;
+  }
+
+  public void setCellDep(CellDep cellDep) {
+    this.cellDep = cellDep;
+  }
+
+  public void setSingleSignCellDep(CellDep singleSignCellDep) {
+    this.singleSignCellDep = singleSignCellDep;
+  }
+
+  public void setMultiSignCellDep(CellDep multiSignCellDep) {
+    this.multiSignCellDep = multiSignCellDep;
+  }
+
+  public void setCodeHash(byte[] codeHash) {
+    this.codeHash = codeHash;
   }
 
   @Override
@@ -86,8 +82,7 @@ public class OmnilockScriptHandler implements ScriptHandler {
     OmnilockWitnessLock omnilockWitnessLock = new OmnilockWitnessLock();
     switch (omnilockConfig.getAuthenticationArgs().getFlag()) {
       case CKB_SECP256K1_BLAKE160:
-        // TODO: refactor adding celldeps
-        txBuilder.addCellDep(secpCellDep(Network.TESTNET));
+        txBuilder.addCellDep(singleSignCellDep);
         omnilockWitnessLock.setSignature(new byte[65]);
         break;
       case ETHEREUM:
@@ -101,6 +96,7 @@ public class OmnilockScriptHandler implements ScriptHandler {
       case DOGECOIN:
         throw new UnsupportedOperationException("Dogecoin");
       case CKB_MULTI_SIG:
+        txBuilder.addCellDep(multiSignCellDep);
         Secp256k1Blake160MultisigAllSigner.MultisigScript multisigScript = omnilockConfig.getMultisigScript();
         Objects.requireNonNull(multisigScript);
         omnilockWitnessLock.setSignature(multisigScript.witnessPlaceholderInLock());
@@ -119,23 +115,6 @@ public class OmnilockScriptHandler implements ScriptHandler {
     int index = scriptGroup.getInputIndices().get(0);
     txBuilder.setWitness(index, WitnessArgs.Type.LOCK, new byte[lock.length]);
     return true;
-  }
-
-  public CellDep secpCellDep(Network network) {
-    OutPoint outPoint = new OutPoint();
-    if (network == Network.MAINNET) {
-      outPoint.txHash = Numeric.hexStringToByteArray("0x71a7ba8fc96349fea0ed3a5c47992e3b4084b031a42264a018e0072e8172e46c");
-      outPoint.index = 0;
-    } else if (network == Network.TESTNET) {
-      outPoint.txHash = Numeric.hexStringToByteArray("0xf8de3bb47d055cdf460d93a2a6e1b05f7432f9777c8c474abf4eec1d4aee5d37");
-      outPoint.index = 0;
-    } else {
-      throw new IllegalArgumentException("Unsupported network");
-    }
-    CellDep cellDep = new CellDep();
-    cellDep.outPoint = outPoint;
-    cellDep.depType = CellDep.DepType.DEP_GROUP;
-    return cellDep;
   }
 
   private boolean buildTransactionForAdministratorMode(AbstractTransactionBuilder txBuilder, ScriptGroup scriptGroup, OmnilockConfig omnilockConfig) {
@@ -157,7 +136,6 @@ public class OmnilockScriptHandler implements ScriptHandler {
       default:
         throw new IllegalArgumentException("Unknown administrator mode " + administratorMode);
     }
-
     OmnilockWitnessLock omnilockWitnessLock = new OmnilockWitnessLock();
     omnilockWitnessLock.setSignature(signature);
     omnilockWitnessLock.setOmnilockIdentity(omnilockConfig.getOmnilockIdentity());
