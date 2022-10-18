e1629ee8f971d23bae15b3ff6d73fd06820b57eb
fjchen7 <fjchen7@gmail.com>
2022-10-18 20:19:36 +0800

    Implement types of light client RPC

---
+++ lightclient/src/main/java/com/nervos/lightclient/type/ScriptDetail.java
@@ -0,0 +1,10 @@
+package com.nervos.lightclient.type;
+
+import org.nervos.ckb.type.Script;
+import org.nervos.ckb.type.ScriptType;
+
+public class ScriptDetail {
+  public Script script;
+  public ScriptType scriptType;
+  public long blockNumber;
+}
