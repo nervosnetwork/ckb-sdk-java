2d45a2de7b0119fa4c17b6b7a59d3955248ae8a0
fjchen7 <fjchen7@gmail.com>
2022-11-03 20:47:44 +0800

    Add getters and setters

--- ckb/src/main/java/org/nervos/ckb/transaction/AbstractInputIterator.java
+++ ckb/src/main/java/org/nervos/ckb/transaction/AbstractInputIterator.java
@@ -27,6 +27,78 @@ public abstract class AbstractInputIterator implements Iterator<TransactionInput
   protected Integer limit = 100;
   protected boolean consumeOffChainCellsFirstly = false;
 
+  public List<TransactionInput> getTransactionInputs() {
+    return transactionInputs;
+  }
+
+  public void setTransactionInputs(List<TransactionInput> transactionInputs) {
+    this.transactionInputs = transactionInputs;
+  }
+
+  public TransactionInput getCurrent() {
+    return current;
+  }
+
+  public void setCurrent(TransactionInput current) {
+    this.current = current;
+  }
+
+  public byte[] getAfterCursor() {
+    return afterCursor;
+  }
+
+  public void setAfterCursor(byte[] afterCursor) {
+    this.afterCursor = afterCursor;
+  }
+
+  public int getInputIndex() {
+    return inputIndex;
+  }
+
+  public void setInputIndex(int inputIndex) {
+    this.inputIndex = inputIndex;
+  }
+
+  public int getSearchKeysIndex() {
+    return searchKeysIndex;
+  }
+
+  public void setSearchKeysIndex(int searchKeysIndex) {
+    this.searchKeysIndex = searchKeysIndex;
+  }
+
+  public List<SearchKey> getSearchKeys() {
+    return searchKeys;
+  }
+
+  public void setSearchKeys(List<SearchKey> searchKeys) {
+    this.searchKeys = searchKeys;
+  }
+
+  public Order getOrder() {
+    return order;
+  }
+
+  public void setOrder(Order order) {
+    this.order = order;
+  }
+
+  public Integer getLimit() {
+    return limit;
+  }
+
+  public void setLimit(Integer limit) {
+    this.limit = limit;
+  }
+
+  public boolean isConsumeOffChainCellsFirstly() {
+    return consumeOffChainCellsFirstly;
+  }
+
+  public void setConsumeOffChainCellsFirstly(boolean consumeOffChainCellsFirstly) {
+    this.consumeOffChainCellsFirstly = consumeOffChainCellsFirstly;
+  }
+
   public AbstractInputIterator addSearchKey(String address) {
     return addSearchKey(address, null);
   }
