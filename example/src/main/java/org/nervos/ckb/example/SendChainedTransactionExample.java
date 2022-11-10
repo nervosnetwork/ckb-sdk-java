aa1c98eb2dad3e578109c89656dad4977180934c
fjchen7 <fjchen7@gmail.com>
2022-11-10 13:15:50 +0800

    Add SendChainedTransactionExample

---
+++ example/src/main/java/org/nervos/ckb/example/SendChainedTransactionExample.java
@@ -0,0 +1,54 @@
+package org.nervos.ckb.example;
+
+import org.nervos.ckb.Network;
+import org.nervos.ckb.service.Api;
+import org.nervos.ckb.service.GsonFactory;
+import org.nervos.ckb.sign.TransactionSigner;
+import org.nervos.ckb.sign.TransactionWithScriptGroups;
+import org.nervos.ckb.transaction.*;
+import org.nervos.ckb.type.TransactionInput;
+import org.nervos.ckb.utils.Numeric;
+
+import java.io.IOException;
+import java.util.Iterator;
+
+public class SendChainedTransactionExample {
+  public static void main(String[] args) throws IOException {
+    Network network = Network.TESTNET;
+    String sender = "ckt1qzda0cr08m85hc8jlnfp3zer7xulejywt49kt2rr0vthywaa50xwsq2qf8keemy2p5uu0g0gn8cd4ju23s5269qk8rg4r";
+
+    TransactionBuilderConfiguration configuration = new TransactionBuilderConfiguration(network);
+    OffChainInputCollector offChainInputCollector = new OffChainInputCollector();
+    AbstractInputIterator iterator = new OffChainInputIterator(
+            new InputIterator(sender),
+            offChainInputCollector,
+            true);
+
+    // The first tx
+    TransactionWithScriptGroups txWithGroups = new CkbTransactionBuilder(configuration, iterator)
+        .addOutput("ckt1qzda0cr08m85hc8jlnfp3zer7xulejywt49kt2rr0vthywaa50xwsq2qf8keemy2p5uu0g0gn8cd4ju23s5269qk8rg4r",
+                   50100000000L)
+        .setChangeOutput(sender)
+        .build();
+    TransactionSigner.getInstance(network)
+        .signTransaction(txWithGroups, "0x6c9ed03816e3111e49384b8d180174ad08e29feb1393ea1b51cef1c505d4e36a");
+
+    Api api = new Api("https://testnet.ckb.dev", false);
+    byte[] txHash = api.sendTransaction(txWithGroups.getTxView());
+    offChainInputCollector.applyOffChainTransaction(api.getTipBlockNumber(), txWithGroups.getTxView());
+    System.out.println("Transaction hash: " + Numeric.toHexString(txHash));
+
+    // The second tx will consume outpoint created by the first tx.
+    txWithGroups = new CkbTransactionBuilder(configuration, iterator)
+            .addOutput("ckt1qzda0cr08m85hc8jlnfp3zer7xulejywt49kt2rr0vthywaa50xwsq2qf8keemy2p5uu0g0gn8cd4ju23s5269qk8rg4r",
+                    100000000000L)
+            .setChangeOutput(sender)
+            .build();
+    TransactionSigner.getInstance(network)
+            .signTransaction(txWithGroups, "0x6c9ed03816e3111e49384b8d180174ad08e29feb1393ea1b51cef1c505d4e36a");
+
+    txHash = api.sendTransaction(txWithGroups.getTxView());
+    offChainInputCollector.applyOffChainTransaction(api.getTipBlockNumber(), txWithGroups.getTxView());
+    System.out.println("Transaction hash: " + Numeric.toHexString(txHash));
+  }
+}
