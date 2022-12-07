74919a6775a311b7fda37f37a32943e95410406f
Liu Chuankai <liuchuankai@cryptape.com>
2022-12-07 16:19:46 +0800

    feat: 🎸 Make ckb-indexer can configure which api url to use

--- ckb-indexer/src/main/java/org/nervos/indexer/CkbIndexerRpcMethods.java
+++ ckb-indexer/src/main/java/org/nervos/indexer/CkbIndexerRpcMethods.java
@@ -2,6 +2,7 @@ package org.nervos.indexer;
 
 public interface CkbIndexerRpcMethods {
   String GET_TIP = "get_tip";
+  String GET_INDEXER_TIP = "get_indexer_tip";
   String GET_CELLS = "get_cells";
   String GET_TRANSACTIONS = "get_transactions";
   String GET_CELLS_CAPACITY = "get_cells_capacity";
